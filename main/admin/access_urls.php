af5f802e9c99540f2b0d71be21db865e33ee2a49
ywarnier <ywarnier@localhost>
2009-08-05 22:26:49 -0500

    Fixed user picture in whoisonline.php when using users subdirs

---
+++ main/admin/access_urls.php
@@ -0,0 +1,208 @@
+<?php
+/*
+==============================================================================
+	Dokeos - elearning and course management software
+
+	Copyright (c) 2009 Dokeos SPRL
+	Copyright (c) 2009 Julio Montoya Armas <gugli100@gmail.com>
+
+	For a full list of contributors, see "credits.txt".
+	The full license can be read in "license.txt".
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	See the GNU General Public License for more details.
+
+	Contact address: Dokeos, rue du Corbeau, 108, B-1030 Brussels, Belgium
+	Mail: info@dokeos.com
+==============================================================================
+*/
+/**
+==============================================================================
+*	@package dokeos.admin
+============================================================================== 
+*/
+
+// name of the language file that needs to be included 
+$language_file = 'admin';
+$cidReset = true;
+require ('../inc/global.inc.php');
+$this_section = SECTION_PLATFORM_ADMIN;
+
+api_protect_admin_script();
+if (!$_configuration['multiple_access_urls'])
+	header('Location: index.php');
+	
+$interbreadcrumb[] = array ("url" => 'index.php', "name" => get_lang('PlatformAdmin'));
+$tool_name = get_lang('MultipleAccessURLs');
+Display :: display_header($tool_name);
+
+require_once (api_get_path(LIBRARY_PATH).'sortabletable.class.php');
+require_once (api_get_path(LIBRARY_PATH).'formvalidator/FormValidator.class.php');
+require_once (api_get_path(LIBRARY_PATH).'security.lib.php');
+require_once (api_get_path(LIBRARY_PATH).'urlmanager.lib.php');
+
+$my_user_url_list = api_get_access_url_from_user(api_get_user_id());
+$current_access_url_id = api_get_current_access_url_id();
+$url_list = UrlManager::get_url_data();
+
+// Actions
+if (isset ($_GET['action'])) {	
+	if ($_GET['action'] == 'show_message')
+		Display :: display_normal_message(Security::remove_XSS(stripslashes($_GET['message'])));	
+		
+	$check = Security::check_token('get');		
+	if ($check) {
+		$url_id=Database::escape_string($_GET['url_id']);
+	
+		switch ($_GET['action']) {		
+			case 'delete_url' :		
+				$result = UrlManager::delete($url_id);					
+				if ($result) {
+					Display :: display_normal_message(get_lang('URLDeleted'));
+				} else {
+					Display :: display_error_message(get_lang('CannotDeleteURL'));
+				}
+				break;
+			case 'lock' :
+				UrlManager::set_url_status('lock',$url_id);				
+				Display :: display_normal_message(get_lang('URLInactive'));
+				break;
+			case 'unlock';
+				UrlManager::set_url_status('unlock',$url_id);
+				Display :: display_normal_message(get_lang('URLActive'));
+				break;			
+			case 'register';			
+				// we are going to register the admin
+				if(api_is_platform_admin()) {					
+					if($current_access_url_id!=-1) {
+						$url_str = '';
+						foreach($url_list as $my_url) {	
+							if (!in_array($my_url['id'],$my_user_url_list)){								
+								UrlManager::add_user_to_url(api_get_user_id(),$my_url['id']);	
+									$url_str.=$my_url['url'].' '; 
+							}
+						}						
+						Display :: display_normal_message(get_lang('AdminUserRegisteredToThisURL').': '.$url_str.'<br />',false);
+					}
+				}						
+				break;	
+			}
+			
+		}
+		Security::clear_token();
+}
+
+$parameters['sec_token'] = Security::get_token();
+
+// checking if the admin is registered in all sites
+
+$url_string='';
+$my_user_url_list = api_get_access_url_from_user(api_get_user_id());
+foreach($url_list as $my_url) {	
+	if (!in_array($my_url['id'],$my_user_url_list)){
+		$url_string.=$my_url['url'].' ';		
+	}	
+}
+if(!empty($url_string)) {
+	Display :: display_warning_message(get_lang('AdminShouldBeRegisterInSite').':<br />'.$url_string,false);
+}
+
+// checking the current installation
+if ($current_access_url_id==-1) {	
+	Display :: display_warning_message(get_lang('URLNotConfiguredPleaseChangedTo').': '.api_get_path(WEB_PATH));	
+} elseif(api_is_platform_admin()) {			
+	$quant= UrlManager::relation_url_user_exist(api_get_user_id(),$current_access_url_id);
+	if ($quant==0) {
+		Display :: display_warning_message('<a href="'.api_get_self().'?action=register&sec_token='.$parameters['sec_token'].'">'.get_lang('ClickToRegisterAdmin').'</a>',false);	
+	} 
+}
+
+//<a href="'.api_get_path(WEB_CODE_PATH).'admin/access_url_edit_sessions_to_url.php">'.Display::return_icon('sessions.gif',get_lang('ManageSessions'),'').get_lang('ManageSessions').'</a>
+// action menu
+echo '<div class="actions" style="height:22px;">';
+echo '<div style="float:right;">
+		<a href="'.api_get_path(WEB_CODE_PATH).'admin/access_url_edit.php">'.Display::return_icon('view_more_stats.gif',get_lang('AddUrl'),'').get_lang('AddUrl').'</a>&nbsp;&nbsp;
+		<a href="'.api_get_path(WEB_CODE_PATH).'admin/access_url_edit_users_to_url.php">'.Display::return_icon('members.gif',get_lang('ManageUsers'),'').get_lang('ManageUsers').'</a>															
+	    <a href="'.api_get_path(WEB_CODE_PATH).'admin/access_url_edit_courses_to_url.php">'.Display::return_icon('courses.gif',get_lang('ManageCourses'),'').get_lang('ManageCourses').'</a>
+	  </div><br />';		  
+echo '</div>';
+
+$table = new SortableTable('urls', 'url_count_mask', 'get_url_data_mask',2); 
+$table->set_additional_parameters($parameters);
+$table->set_header(0, '', false);
+
+$table->set_header(1, get_lang('URL'));
+$table->set_header(2, get_lang('Description'));
+$table->set_header(3, get_lang('Active'));
+//$table->set_header(4, get_lang('Status'));
+$table->set_header(4, get_lang('Modify'));
+
+$table->set_column_filter(3, 'active_filter');
+//$table->set_column_filter(4, 'status_filter');
+$table->set_column_filter(4, 'modify_filter');
+
+//$table->set_form_actions(array ('delete' => get_lang('DeleteFromPlatform')));
+$table->display(); 
+/*
+function status_filter($active, $url_params, $row) {	
+	$url_id =UrlManager::get_url_id($row[1]);	
+	if ($row[0] == $url_id ) { 	
+		$action='lock';
+		$image='right';
+	} else {
+		$image='wrong';
+	}
+	// you cannot lock the default	
+	$result = Display::return_icon($image.'.gif', get_lang(ucfirst($action)));		
+
+	return $result;
+}
+*/
+function modify_filter($active, $url_params, $row) {
+	global $charset;	
+	$url_id = $row['0'];
+	$result .= '<a href="access_url_edit.php?url_id='.$url_id.'">'.Display::return_icon('edit.gif', get_lang('Edit')).'</a>&nbsp;';
+	if ($url_id != '1') {	
+		$result .= '<a href="access_urls.php?action=delete_url&amp;url_id='.$url_id.'&amp;sec_token='.$_SESSION['sec_token'].'" onclick="javascript:if(!confirm('."'".addslashes(api_htmlentities(get_lang("ConfirmYourChoice"),ENT_QUOTES,$charset))."'".')) return false;">'.Display::return_icon('delete.gif', get_lang('Delete')).'</a>';
+	}
+	return $result;
+}
+
+function active_filter($active, $url_params, $row) {	
+	$active = $row['3'];
+	if ($active=='1') {
+		$action='lock';
+		$image='right';
+	}
+	if ($active=='0') {
+		$action='unlock';
+		$image='wrong';
+	}
+	// you cannot lock the default
+	if ($row['0']=='1') { 
+		$result = Display::return_icon($image.'.gif', get_lang(ucfirst($action)));
+	} else {
+		$result = '<a href="access_urls.php?action='.$action.'&amp;url_id='.$row['0'].'&amp;sec_token='.$_SESSION['sec_token'].'">'.Display::return_icon($image.'.gif', get_lang(ucfirst($action))).'</a>';		
+	}
+	return $result;
+}
+
+// this 2 "mask" function are here just because the SortableTable
+function get_url_data_mask($id, $url_params=null, $row=null) {
+	return UrlManager::get_url_data();
+}
+function url_count_mask() {
+	return UrlManager::url_count();				
+}
+
+/*
+==============================================================================
+		FOOTER 
+==============================================================================
+*/
+Display :: display_footer();
+?>
---
+++ main/inc/lib/fckeditor/myconfig.js
@@ -0,0 +1,118 @@
+/*
+ *	Dokeos - elearning and course management software
+ *
+ *	Copyright (c) 2008-2009 Dokeos SPRL
+ *	Copyright (c) 2008-2009 Julio Montoya
+ *	Copyright (c) 2008-2009 Juan Carlos Raña
+ *	Copyright (c) 2008-2009 Ivan Tcholakov
+ *
+ *	For a full list of contributors, see "credits.txt".
+ *	The full license can be read in "license.txt".
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License
+ *	as published by the Free Software Foundation; either version 2
+ *	of the License, or (at your option) any later version.
+ *
+ *	See the GNU General Public License for more details.
+ *
+ * Contact address: Dokeos, rue du Corbeau, 108, B-1030 Brussels, Belgium
+ * Mail: info@dokeos.com
+ */
+
+/*
+ * Custom editor configuration settings.
+ *
+ * Follow this link for more information:
+ * http://docs.fckeditor.net/FCKeditor_2.x/Developers_Guide/Configuration/Configuration_Options
+ *
+ * Please, do not modify the file fckconfig.js in order to make upgrades easy.
+ * Just create your desired settings in this file, myconfig.js.
+ * Also, configuration options (with higher priority) may be created/modified within the file myconfig.php.
+ */
+
+FCKConfig.DocType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' ;
+FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/silver/' ;
+
+FCKConfig.ProtectedSource.Add( /<script[\s\S]*?\/script>/gi ) ;	// To protect <script> tags.
+
+FCKConfig.AutoDetectLanguage = false ;
+
+FCKConfig.ContextMenu = ['Generic','Link','Anchor','Image','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ;
+FCKConfig.TemplatesXmlPath	= FCKConfig.EditorPath + 'fcktemplates.xml.php' ;
+
+FCKConfig.DisableFFTableHandles = false ;
+
+FCKConfig.SmileyWindowWidth = 450 ;
+FCKConfig.SmileyWindowHeight = 250 ;
+
+
+/*
+ * Plugins.
+ */
+
+// Loading integrated by the Dokeos team plugins. To enable/disable them, see myconfig.php.
+FCKConfig.AvailableLanguages = 'en,af,ar,bg,bn,bs,ca,cs,da,de,el,en-au,en-ca,en-uk,eo,es,et,eu,fa,fi,fo,fr-ca,fr,gl,gu,he,hi,hr,hu,is,it,ja,km,ko,lt,lv,mn,ms,nb,nl,no,pl,pt-br,pt,ro,ru,sk,sl,sr-latn,sr,sv,th,tr,uk,vi,zh-cn,zh' ;
+FCKConfig.LoadPlugin = eval( '(' + FCKConfig.PageConfig.LoadPlugin + ')' ) ;
+for ( var i = 0 ; i < FCKConfig.LoadPlugin.length ; i++ ) {
+	switch ( FCKConfig.LoadPlugin[i] ) {
+		// These plugins do not need language files or they load lannguage files in their own way.
+		case 'dragresizetable':
+		case 'tablecommands':
+		case 'ImageManager':
+		case 'prompt':
+			FCKConfig.Plugins.Add( FCKConfig.LoadPlugin[i] ) ;
+			break ;
+		default:
+			// The rest of the plugins require loading language files.
+			FCKConfig.Plugins.Add( FCKConfig.LoadPlugin[i], FCKConfig.AvailableLanguages ) ;
+	}
+}
+
+// API-key for the "googlemaps" plugin.
+// The following key is valid for http://localhost. You must get one for each server where you want to use
+// the plugin, just get the key for free here after agreeing to the Terms of Use of the GoogleMaps API:
+// http://www.google.com/apis/maps/signup.html.
+// If you leave an empty string then the toolbar icon won't be shown.
+FCKConfig.GoogleMaps_Key = 'ABQIAAAAlXu5Pw6DFAUgqM2wQn01gxT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSy5hTGQdsosYD3dz6faZHVrO-02A' ;
+
+// This is the old flash plugin. Now the editor has a built-in flash dialog.
+// Probably this plugin will be removed at the next release.
+//FCKConfig.Plugins.Add('Flash', 'en') ;
+
+// You may add your own plugins here, i.e. write something as follows:
+// FCKConfig.Plugins.Add('my_plugin', 'en') ;
+
+
+/*
+ * File Manager.
+ */
+
+//Upload path for the Image manager. Leave it empty.
+FCKConfig.IMUploadPath = '' ;
+
+
+/*
+ * Quick-upload tabs.
+ */
+
+// Lists of allowed and denied name extensions of files to be uploaded.
+// for all acceptable types of files;
+FCKConfig.LinkUploadAllowedExtensions	= '.(7z|aiff|asf|avi|bmp|csv|doc|fla|flv|gif|gz|gzip|jpeg|jpg|mid|mov|mp3|mp4|mpc|mpeg|mpg|ods|odt|pdf|png|ppt|pxd|qt|ram|rar|rm|rmi|rmvb|rtf|sdc|sitd|swf|sxc|sxw|tar|tgz|tif|tiff|txt|vsd|wav|wma|wmv|xls|xml|zip)$' ;			// empty for all
+FCKConfig.LinkUploadDeniedExtensions	= '' ;
+// for images;
+FCKConfig.ImageUploadAllowedExtensions	= '.(jpg|gif|jpeg|png|bmp)$' ;
+FCKConfig.ImageUploadDeniedExtensions	= '' ;
+// for flash objects;
+FCKConfig.FlashUploadAllowedExtensions	= '.(swf)$' ;
+FCKConfig.FlashUploadDeniedExtensions	= '' ;
+// for audio files;
+FCKConfig.MP3UploadAllowedExtensions	= '.(mp3)$' ;
+FCKConfig.MP3UploadDeniedExtensions		= '' ;
+// for video files;
+FCKConfig.VideoUploadAllowedExtensions	= '.(mpg|mpeg|mp4|avi|wmv|mov|asf)$' ;
+FCKConfig.VideoUploadDeniedExtensions	= '' ;
+// for video (flv) files.
+FCKConfig.MediaUploadAllowedExtensions	= '.(flv|mp4)$' ;
+FCKConfig.MediaUploadDeniedExtensions	= '' ;
+// Note: These lists get combined with the platform's white and black lists.
