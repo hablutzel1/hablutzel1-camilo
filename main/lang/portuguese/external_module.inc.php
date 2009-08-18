34400046a1b891c6589353c10ad6ffa19afaec4b
jmontoya <jmontoya@localhost>
2009-08-18 15:52:05 -0500

    Minor - Adding remove_XSS function

---
+++ main/lang/portuguese/external_module.inc.php
@@ -0,0 +1,27 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langLinkSite = "Ligação a uma página web";
+$langSubTitle = "Acrescenta uma ligação de uma página web na página principal do curso. Para fazer isso, vá a essa página web, copie o endereço da URL na barra do browser e cole-o no campo \"Ligação\" aqui em baixo.";
+$langAddPage            = "Acrescentar página";
+$langSendPage           = "Página a enviar";
+$langCouldNot           = "O ficheiro não pode ser enviado";
+$langOkSentLink         = "A sua ligação/link foi adicionada. Encontra-se agora acessível a partir da <a href=\"%s/index.php\">Página inicial</a>";
+$langTooBig             = "Não escolheu o ficheiro que pretende enviar, ou então é demasiado grande";
+$langExplanation        = "A página deve estar em formato HTML (p. e. \"arame.htm\"). Será acessível através duma ligação colocada na página inicial. Se quiser enviar documentos que não sejam HTML (PDF, Word, Power Point, Vídeo, etc.) utilize a <a href=../document/document.php>ferramenta Documentos</a>";
+$langPgTitle            = "Título da página";
+$langNoLinkURL          = "Por favor, escreva a ligação da URL";
+$langLinkTarget = "Publicar";
+$langSameWindow = "Na mesma janela";
+$langNewWindow = "Numa nova janela";
+$langAdded = "A ligação foi acrescentada";
+$langAddLink = "Acrescentar uma ligação";
+$langNoLinkName = "Por favor, escreva o nome da ligação";
+$langEditLink = "Editar Link da Página do Curso";
+$langChangePress = "Altere e clique em OK";
+$langLinkChanged = "Course Homepage Link changed. Use breadcrumb menu above to return to the homepage.";
+$NoLinkName = "Ligação sem nome";
+$NoLinkURL = "Ligação sem endereço (URL)";
+$LinkChanged = "Ligação alterada";
+$OkSentLink = "A ligação foi enviada";
+?>
