c40f2d7cbd6d140bb16d45ef86e4572f22a679ff
herodoto <herodoto@localhost>
2009-11-03 22:29:55 +0100

    fix access to a course from platform admin - list courses

---
+++ main/lang/thai/course_description.inc.php
@@ -0,0 +1,15 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langCourseProgram  = "คำอธิบายรายวิชา";
+$langThisCourseDescriptionIsEmpty  = "วิชานี้ยังไม่มีคำอธิบายรายวิชา";
+$langEditCourseProgram  = "สร้างและแก้ไขคำอธิบายรายวิชา";
+$QuestionPlan  = "ฝากคำถามถึงหัวหน้า";
+$langInfo2Say  = "ส่งข้อมูลและสาระถึงผู้เรียน";
+$langOuAutreTitre  = "เรื่อง";
+$langNewBloc  = "อื่นๆ";
+$langAddCat  = "เพิ่มประเภท";
+$langAdd  = "เพิ่ม";
+$langValid  = "ถูกต้อง";
+$langBackAndForget  = "ย้อนกลับ";
+?>
