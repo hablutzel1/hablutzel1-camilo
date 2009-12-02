b8c450d675f578e83c3ed3b60ee21501186fd316
aportugal <aportugal@localhost>
2009-12-02 17:05:27 -0500

    Fixed the file Statistics.lib.test.php for all.test2.php CT#191

---
+++ main/lang/thai/userInfo.inc.php
@@ -0,0 +1,31 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langLineNumber = "บรรทัดที่";
+$langLine = "บรรทัด";
+$langLines = "บรรทัด";
+$langLineOrLines = "บรรทัด";
+$langMoveUp = "เลื่อนขึ้น";
+$langMoveDown = "เลื่อนลง";
+$langAddNewHeading = "เพิ่มหัวเรื่องใหม่";
+$langCourseAdministratorOnly = "ผู้สอนเท่านั้น";
+$langDefineHeadings = "นิยามหัวเรื่อง";
+$langBackToUsersList = "กลับไปที่รายชื่อผู้ใช้";
+$langTracking = "ติดตามผล";
+$langCourseManager = "อาจารย์ผู้สอน";
+$langModRight = "เปลี่ยนแปลงสิทธิ์ของ";
+$langNoAdmin = "ตั้งแต่นี้ไป<b>ไม่มี</b>สิทธิ์ในหน้านี้ ";
+$langAllAdmin = "ตั้งแต่นี้ไป มีสิทธิ์ในหน้านี้<b>ทั้งหมด</b> ";
+$langModRole = "เปลี่ยนบทบาทของ";
+$langRole = "บทบาท/สถานภาพ";
+$langIsNow = "ตั้งแต่นี้ไป";
+$langInC = "ในรายวิชานี้";
+$langFilled = "ไม่ได้กรอกรายการในทุกช่อง";
+$langUserNo = "ผู้ใช้เลขที่";
+$langTaken = "กำลังใช้งานอยู่ โปรดเลือกอย่างอื่น";
+$langTutor = "ผู้ช่วยสอน";
+$langUnreg = "ถอนการสมัคร";
+$langGroupUserManagement = "การบริหารจัดการกลุ่ม";
+$langUnregister = "ไม่ได้ลงทะเบียน";
+$langAddAUser = "เพิ่มผู้ใช้";
+?>
