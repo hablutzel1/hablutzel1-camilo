b5ad99599cf266aa491deba3306225a2071e510e
Julio Montoya <Julio Montoya@localhost>
2009-12-22 12:37:37 -0500

    Tuning social network + CSS see #190

---
+++ main/messages/view_message.php
@@ -0,0 +1,101 @@
+<?php
+/* For licensing terms, see /chamilo_license.txt */
+/**
+*	@package dokeos.messages
+*/
+
+
+$language_file = array('registration','messages','userInfo');
+$cidReset= true;
+require_once '../inc/global.inc.php';
+api_block_anonymous_users();
+if (api_get_setting('allow_message_tool')!='true'){
+	api_not_allowed();
+}
+require_once api_get_path(LIBRARY_PATH).'message.lib.php';
+
+
+/*
+==============================================================================
+		HEADER
+==============================================================================
+*/
+if ($_REQUEST['f']=='social') {
+	$this_section = SECTION_SOCIAL;
+	$interbreadcrumb[]= array ('url' => api_get_path(WEB_PATH).'main/social/profile.php','name' => get_lang('Social'));
+	$interbreadcrumb[]= array ('url' => 'inbox.php?f=social','name' => get_lang('Inbox'));	
+} else {
+	$this_section = SECTION_MYPROFILE;
+	$interbreadcrumb[]= array ('url' => '#','name' => get_lang('Profile'));
+	$interbreadcrumb[]= array ('url' => 'inbox.php','name' => get_lang('Inbox'));
+}
+$interbreadcrumb[]= array ('url' => '#','name' => get_lang('View'));
+Display::display_header('');
+
+if ($_GET['f']=='social') {
+	echo '<div class="actions-title">';
+	echo get_lang('Messages');
+	echo '</div>';
+	$social_parameter = '?f=social';
+} else {
+	if (api_get_setting('extended_profile') == 'true') {
+		echo '<div class="actions">';
+		
+		if (api_get_setting('allow_social_tool') == 'true' && api_get_setting('allow_message_tool') == 'true') {
+			echo '<a href="'.api_get_path(WEB_PATH).'main/social/profile.php">'.Display::return_icon('shared_profile.png', get_lang('ViewSharedProfile')).'&nbsp;'.get_lang('ViewSharedProfile').'</a>';
+		}
+		if (api_get_setting('allow_message_tool') == 'true') {
+			echo '<a href="'.api_get_path(WEB_PATH).'main/messages/inbox.php">'.Display::return_icon('inbox.png').' '.get_lang('Messages').'</a>';
+		}	
+		echo '<a href="'.api_get_path(WEB_PATH).'main/auth/profile.php?type=reduced">'.Display::return_icon('edit.gif', get_lang('EditNormalProfile')).'&nbsp;'.get_lang('EditNormalProfile').'</a>';
+		
+		echo '</div>';
+	}
+}
+
+echo '<div id="inbox-wrapper">';
+	//LEFT COLUMN
+	if (api_get_setting('allow_social_tool') != 'true') { 
+		echo '<div id="inbox-menu" class="actions">';
+		echo '<ul>';
+			echo '<li><a href="'.api_get_path(WEB_PATH).'main/messages/inbox.php">'.Display::return_icon('inbox.png',get_lang('Inbox')).get_lang('Inbox').'</a>'.'</li>';
+			echo '<li><a href="'.api_get_path(WEB_PATH).'main/messages/new_message.php">'.Display::return_icon('message_new.png',get_lang('ComposeMessage')).get_lang('ComposeMessage').'</a>'.'</li>';
+			echo '<li><a href="'.api_get_path(WEB_PATH).'main/messages/outbox.php">'.Display::return_icon('outbox.png',get_lang('Outbox')).get_lang('Outbox').'</a>'.'</li>';
+		echo '</ul>';
+		echo '</div>';
+	} else {
+		require_once api_get_path(LIBRARY_PATH).'social.lib.php';
+		SocialManager::show_social_menu('messages');		
+	}
+
+	echo '<div id="inbox">';
+		//MAIN CONTENT
+		
+		if (empty($_GET['id'])) {
+			$id_message = $_GET['id_send'];
+			$source = 'outbox';
+		} else {
+			$id_message = $_GET['id'];
+			$source = 'inbox';
+		}
+		
+		$message = MessageManager::show_message_box($id_message,$source);
+		if (!empty($message)) {
+			echo $message;
+			
+	
+		} else {
+			api_not_allowed();
+		}
+
+	echo '</div>';
+
+echo '</div>';
+
+/*
+==============================================================================
+		FOOTER
+==============================================================================
+*/
+Display::display_footer();
+?>
---
+++ main/social/home.php
@@ -0,0 +1,98 @@
+<?php //$id: $
+/* For licensing terms, see /chamilo_license.txt */
+/**
+ * @package dokeos.social
+ * @author Julio Montoya <gugli100@gmail.com>
+ */
+ 
+$language_file = array('registration','messages','userInfo','admin','forum','blog');
+$cidReset = true;
+require '../inc/global.inc.php';
+require_once api_get_path(LIBRARY_PATH).'usermanager.lib.php';
+require_once api_get_path(LIBRARY_PATH).'social.lib.php';
+require_once api_get_path(LIBRARY_PATH).'array.lib.php';
+require_once api_get_path(LIBRARY_PATH).'group_portal_manager.lib.php';
+
+$user_id = api_get_user_id();
+$show_full_profile = true;
+//social tab
+$this_section = SECTION_SOCIAL;
+
+api_block_anonymous_users();
+$htmlHeadXtra[] = '<script src="'.api_get_path(WEB_LIBRARY_PATH).'javascript/jquery.js" type="text/javascript" language="javascript"></script>'; //jQuery
+
+
+Display :: display_header(null);
+
+echo '<div id="social_wrapper">';
+
+	//this include the social menu div
+	SocialManager::show_social_menu(array('messages'));	
+	
+	echo '<div id="social_main">';
+	
+		echo '<div id="social_main_sub">';
+		
+			echo '<div id="social_top">';
+			echo get_lang('User Online').'120';
+			echo '</div>';
+		
+			echo '<div id="social_left">';
+			echo 'myinfo';
+			
+				echo '<div id="social_center">';
+				echo '</div>';	
+				echo '<div id="social_center">';
+				echo '</div>';
+				echo '<div id="social_center">';
+				echo '</div>';
+				
+			echo '</div>';
+			
+			echo '<div id="social_right">';
+					
+				$results = GroupPortalManager::get_groups_by_age(1);
+				
+				$groups = array();
+				foreach ($results as $result) {
+					
+					$id = $result['id'];
+					$url_open  = '<a href="groups.php?id='.$id.'">';
+					$url_close = '</a>';		
+					$groups[]= array($url_open.$result['picture_uri'].$url_close, $url_open.$result['name'].$url_close, cut($result['description'],180,true));
+				}
+				if (count($groups) > 0) {		
+					echo '<h3>'.get_lang('Popular').'</h3>';	
+					Display::display_sortable_grid('newest', array(), $groups, array('hide_navigation'=>true, 'per_page' => 100), $query_vars, false, array(true, true, true,false));		
+				}
+				
+				
+				$results = GroupPortalManager::get_groups_by_popularity(1);
+				$groups = array();
+				foreach ($results as $result) {
+					$id = $result['id'];
+					$url_open  = '<a href="groups.php?id='.$id.'">';
+					$url_close = '</a>';		
+					
+					if ($result['count'] == 1 ) {
+						$result['count'] = $result['count'].' '.get_lang('Member');	
+					} else {
+						$result['count'] = $result['count'].' '.get_lang('Members');
+					}
+					
+					$groups[]= array($url_open.$result['picture_uri'].$url_close, $url_open.$result['name'].$url_close,$result['count'],cut($result['description'],120,true));
+				}
+				if (count($groups) > 0) {
+					echo '<h3>'.get_lang('Popular').'</h3>';
+					Display::display_sortable_grid('popular', array(), $groups, array('hide_navigation'=>true, 'per_page' => 100), $query_vars, false, array(true, true, true,true,true));
+				}
+			
+			echo '</div>';
+			
+		echo '</div>';
+		
+	echo '</div>';
+	
+echo '</div>';
+
+Display :: display_footer();
