c397fce8621542defed4258f47dac5f597b02bfe
cvargas <cvargas@localhost>
2009-12-31 11:43:15 -0500

    list of users who have not given the task see bt#487

---
+++ main/messages/index.php
@@ -0,0 +1,10 @@
+<?php
+/* For licensing terms, see /chamilo_license.txt */
+require_once '../inc/global.inc.php';
+if (api_get_setting('allow_social_tool')=='true' &&  api_get_setting('allow_message_tool')=='true') {
+	header('Location:inbox.php?f=social');	
+} elseif ( api_get_setting('allow_message_tool')=='true') {
+	header('Location:inbox.php');
+}
+exit;
+?>
