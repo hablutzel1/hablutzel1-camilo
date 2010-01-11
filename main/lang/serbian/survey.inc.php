850048e9f0cfbf42371d90898563b6b1e9e9d477
cvargas <cvargas@localhost>
2010-01-11 12:15:27 -0500

    improve index admin in style chamilo see CT#462

---
+++ main/lang/serbian/survey.inc.php
@@ -0,0 +1,30 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$PublishSurvey = "Objavi anketu";
+$CompareQuestions = "Uporedi pitanja";
+$InformationUpdated = "Informacija ažurirana";
+$SurveyTitle = "Naslov ankete";
+$SurveyIntroduction = "Uvod u anketu";
+$CreateNewSurvey = "Napravi novu anketu";
+$Survey = "Anketa";
+$SurveyTemplate = "Šablon ankete";
+$PleaseEnterSurveyTitle = "Molimo Vas unesite naslov ankete";
+$PleaseEnterValidDate = "Unesite ispravan datum";
+$NotPublished = "Nije objavljeno";
+$AdvancedReportDetails = "Opcija napredno izveštavanje vam omugu&#263;ava odabir korisnika i pitanja za ta&#269;niji uvid ";
+$AdvancedReport = "Napredno izveštavanje";
+$CompleteReportDetails = "Opcija Kompletno izveštavanje prikazuje sve rezultate ispitanih korisnika i omogu&#263;ava prenos rezultata u csv format (za Excel) ";
+$CompleteReport = "Kompletno izveštavanje";
+$OnlyThoseAddresses = "Pošalji ankete jedino na ove adrese";
+$BackToQuestions = "Nazad na pitanja";
+$SelectWhichLanguage = "Izaberite jezik u kojem bi trebalo da se kreiraju ankete";
+$CreateInAnotherLanguage = "Kreiraj ovo istraživanje na drugom jeziku";
+$ExportInExcel = "Izvoz u Ekscel formatu";
+$ComparativeResults = "Komparativni rezultati";
+$SelectDataYouWantToCompare = "Izaberite podatke koje želite da uporedite";
+$ClickHereToOpenSurvey = "Kliknite ovde da ispunite anketu";
+$SurveyNotShared = "Ankete jos nisu bile sharovane";
+$ViewSurvey = "Pogledaj anketu";
+$SelectDisplayType = "Odaberi oblik prikaza : ";
+?>
