4247de852876b3dae2cb5a47f8983878982643e0
Julio Montoya <Julio Montoya@localhost>
2010-01-20 16:28:49 -0500

    Minor - width table changed

---
+++ main/lang/vietnamese/admin.inc.php
@@ -0,0 +1,42 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$AdminBy  = "Quản trị bởi ";
+$AdministrationTools  = "Các công cụ quản trị";
+$State  = "Tình trạng hệ thống";
+$Statistiques  = "Thống kê";
+$langUsed       = "used";
+$langPresent   = "Ok";
+$langMissing    = "missing";
+$langExist      = "exists";
+$DisplayTeacherInCourselistTitle = "Tên giáo viên trên tiêu đề khóa học";
+$DisplayTeacherInCourselistComment = "Tên Giáo viên trong chú thích danh sách khóa học";
+$langAddAdminInApache  = "Thêm người quản trịr";
+$langAddFaculties  = "Thêm bộ môn";
+$langSearchACourse   = "Tìm giáo trình";
+$langSearchAUser   = "Tìm người dùngr";
+$langTechnicalTools  = "Quản trị kỹ thuật";
+$langConfig  = "Cầu hình hệ thống";
+$langLogIdentLogoutComplete  = "Danh mục Login (mở rộng)";
+$langNomOutilTodo = "Quản lý danh mục cần l� m";
+$langNomPageAdmin = "Quản trị";
+$langSysInfo = "Thông tin về hệ thống";
+$langDiffTranslation = "So sánh các bản dịch ngôn ngữ";
+$langStatOf = "Thống kê về ";
+$langSpeeSubscribe = "Đăng ký nhanh quyền kiểm tra giáo trình";
+$langLogIdentLogout = "Danh mục Login ";
+$langServerStatus = "Trạng thái MySQL server�: ";
+$langDataBase = "Database ";
+$langRun = "work";
+$langClient = "MySql Client ";
+$langServer = "MySql Server ";
+$langtitulary = "Owner";
+$langUpgradeBase = "Upgrade database";
+$langManage = "Quản lý chuyên môn";
+$langErrorsFound = "lỗi tìm thấy";
+$langMaintenance = "Bảo trì";
+$langUpgrade = "Nâng cấp";
+$langWebsite = "Claroline website";
+$langDocumentation = "T� i liệu";
+$langContribute = "T� i trợ";
+?>
