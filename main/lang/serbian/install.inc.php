05f993534357d4ce45734046059651412c65e02d
cvargas <cvargas@localhost>
2010-02-05 17:13:26 -0500

    Create test search CT#191

---
+++ main/lang/serbian/install.inc.php
@@ -0,0 +1,55 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langStatDB              = "Pra&#263;enje baze.";
+$langEnableTracking      = "Omogu&#263;i pra&#263;enje";
+$langInstituteShortName  = "Kratko ime organizacije";
+$langWarningResponsible  = "Koristi ovaj skript samo posle backup-a. Chamilo tim ne odgovara za izgubljene ili uništene podatke";
+$langAllowSelfRegProf  = "Dozvoli samoregistraciju za kreatora kursa";
+$langEG = "pr.";
+$langDBHost = "Host baze podataka";
+$langDBLogin = "Korisni&#269;ko ime baze podataka";
+$langDBPassword = "Lozinka baze podataka";
+$langMainDB = "Glavna Chamiloova baza podataka";
+$langAllFieldsRequired = "sva polja su obavezna";
+$langPrintVers = "Verzija za štampu";
+$langLocalPath = "Odgovaraju&#263;a lokalna putnja";
+$langAdminEmail = "E-mail administratora";
+$langAdminName = "Ime administratora";
+$langAdminSurname = "Prezime administratora";
+$langAdminLogin = "Korisni&#269;ko ime administratora";
+$langAdminPass = "Lozinka administratora (<font color=\"red\">možda &#263;ete ovo želeti da promenite</font>)";
+$langEducationManager = "Administrator projekta";
+$langCampusName = "Ime vašeg portala";
+$langDBSettingIntro = "Instalacioni skript &#263;e da kreira Chamiloovu glavnu bazu. Molim Vas, obratite pažnju da &#263;e Chamilo morati da kreira više baza. Ako Vam vaš Hosting servis dozvoljava da koristite samo jednu bazu Chamilo ne&#263;e raditi osim ako ne odaberete opciju „Jedna baza\".";
+$langStep1 = "Korak 1 od 6";
+$langStep2 = "Korak 2 od 6";
+$langStep3 = "Korak 3 od 6";
+$langStep4 = "Korak 4 od 6";
+$langStep5 = "Korak 5 od 6";
+$langStep6 = "Korak 6 od 6";
+$langCfgSetting = "Konfiguraciona podešavanja";
+$langDBSetting = "Podešavanja MySQL baze";
+$langMainLang = "Glavni jezik";
+$langLicence = "Licenca";
+$langLastCheck = "Poslednja provera pre instaliranja";
+$langRequirements = "Zahtevi";
+$langDbPrefixForm = "Prefiks MySQL baze";
+$langDbPrefixCom = "Ostavi prazno ako nije zahtevano druga&#269;ije";
+$langEncryptUserPass = "Šifruj korisni&#269;ke lozinke u bazi";
+$langSingleDb = "Koristi jednu ili više baza za Chamilo";
+$langAllowSelfReg = "Omogu&#263;i samostalno registrovanje";
+$langRecommended = "(preporu&#269;eno)";
+$langScormDB = "Scorm baza";
+$langAdminLastName = "Prezime administratora";
+$langAdminFirstName = "Ime administratora";
+$langAdminPhone = "Telefon administratora";
+$langInstituteURL = "URL ove organizacije";
+$langDokeosURL = "URL Chamiloa";
+$langUserDB = "Baza korisnika";
+$ReadThoroughly = "Pro&#269;itajte pažljivo.";
+$DokeosNeedFollowingOnServer = "Da bi Chamilo radio potrebno je da imate slede&#263;e na vašem serveru:";
+$WarningExistingDokeosInstallationDetected = "Upozorenje!<br>Instaler je otkrio postoje&#263;u Chamilo platformu na vašem sistemu.";
+$NewInstallation = "Nova Instalacija";
+$CheckDatabaseConnection = "Proveri konekciju sa bazom";
+?>
