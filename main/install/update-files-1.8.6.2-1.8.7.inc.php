050ac6242788239521602ffe4deb65ee66db35d1
cvargas <cvargas@localhost>
2010-02-16 11:43:16 -0500

    Fixed Changing the password had been a mistake, if he was wrong CT#602

---
+++ main/install/update-files-1.8.6.2-1.8.7.inc.php
@@ -0,0 +1,24 @@
+<?php
+/* See license terms in /license.txt */
+/**
+==============================================================================
+* Updates the Chamilo files from version 1.8.6.2 to version 1.8.7
+* This script operates only in the case of an update, and only to change the
+* active version number (and other things that might need a change) in the
+* current configuration file.
+* @package chamilo.install
+==============================================================================
+*/
+require_once '../inc/lib/main_api.lib.php';
+require_once '../inc/lib/fileUpload.lib.php';
+require_once '../inc/lib/database.lib.php';
+
+if (defined('SYSTEM_INSTALLATION') || defined('DOKEOS_COURSE_UPDATE')) {
+
+	// Start coding here...
+
+} else {
+
+	echo 'You are not allowed here !';
+
+}
