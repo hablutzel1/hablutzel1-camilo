68932d3c4c1fd6e42bb6b8ef2e3d966b69e4d525
Ivan Tcholakov <Ivan Tcholakov@localhost>
2010-03-02 23:32:48 +0200

    Feature #272 - Minor corrections within a test.

---
+++ main/lang/latvian/course_home.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langActivate = "Rādīt";
+$langDeactivate = "Slēpt";
+$langInLnk = "Slēptie rīki un saites";
+$langDelLk = "Vai tie";
+$langCourseCreate = "Izveidot kursa mājas lapu";
+$langNameOfTheLink = "Vientnes (saites) nosaukums";
+$lang_main_categories_list = "Galvenais nodaļu saraksts";
+$langCourseAdminOnly = "Tikai docētājiem";
+$PlatformAdminOnly = "Tikai Platformas administratoriem";
+$langCombinedCourse = "Kombinētais kurss";
+$ToolIsNowVisible = "Rīks ir redzams";
+$ToolIsNowHidden = "Rīks ir slēpts";
+$EditLink = "Labot vietni (saiti)";
+$Forum = "Forums";
+$Administration = "Administr?";
+?>
