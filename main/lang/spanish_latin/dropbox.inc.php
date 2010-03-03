c3ba5f92980da987cdecd20b5c851d34f5568a3a
cfasanando <cfasanando@localhost>
2010-03-03 16:39:04 -0500

    minor - added email inside information about teachers followed by drh - partial CT#603

---
+++ main/lang/spanish_latin/dropbox.inc.php
@@ -0,0 +1,53 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$ReceivedFiles = "Archivos recibidos";
+$SentFiles = "Archivos enviados";
+$ReceivedTitle = "Título";
+$SentTitle = "Título";
+$Authors = "Autores";
+$Size = "Tamaño";
+$LastResent = "Último reenvío";
+$kB = "kB";
+$Root = "Raíz";
+$NoSentFilesHere = "No hay archivos <b>enviados</b> que mostrar aquí.";
+$NoReceivedFilesHere = "No hay archivos <b>recibidos</b>.";
+$UploadNewFile = "Enviar un nuevo archivo";
+$AreYouSureToDelete = "Está seguro de querer borrar";
+$Feedback = "Feedback";
+$CloseFeedback = "Cerrar feedback";
+$AddNewFeedback = "Añadir un nuevo feedback";
+$DropboxFeedbackStored = "El mensaje de feedback ha sido almacenado";
+$AllUsersHaveDeletedTheFileAndWillNotSeeFeedback = "Todos los usuarios han borrado el archivo por lo que nadie podrá ver el feedback que está añadiendo.";
+$FeedbackError = "Error de feedback";
+$PleaseTypeText = "Por favor, escriba algún texto.";
+$YouAreNotAllowedToDownloadThisFile = "No tiene permiso para descargar este archivo.";
+$CheckAtLeastOneFile = "Compurebe al menos un archivo.";
+$ReceivedFileDeleted = "El archivo recibido ha sido borrado.";
+$SentFileDeleted = "El archivo enviado ha sido borrado.";
+$FilesMoved = "Los archivos seleccionados han sido movidos.";
+$ReceivedFileMoved = "Los archivos recibidos han sido movidos.";
+$SentFileMoved = "Los archivos enviados han sido movidos";
+$NotMovedError = "Este archivo(s) no puede moverse.";
+$AddNewCategory = "Añadir una nueva categoría";
+$EditCategory = "Editar esta categoría";
+$CategoryName = "Nombre de la categoría";
+$ErrorPleaseGiveCategoryName = "Por favor, escoja un nombre para la categoría";
+$CategoryAlreadyExistsEditIt = "Esta categoría ya existe, por favor use un nombre diferente";
+$CurrentlySeeing = "Actualmente está viendo la categoría";
+$CategoryStored = "La categoría ha sido guardada.";
+$CategoryModified = "La categoría ha sido modificada.";
+$CategoryDeleted = "La categoría ha sido borrada.";
+$AuthorFieldCannotBeEmpty = "El campo autor no puede estar vacío";
+$YouMustSelectAtLeastOneDestinee = "Debe seleccionar al menos un destino";
+$InvalidUserDetected = "Detectado un usuario inválido";
+$InvalidGroupDetected = "Detectado un grupo inválido";
+$DropboxFileTooBig = "El archivo del baúl de tareas es demasiado grande.";
+$TheFileIsNotUploaded = "El archivo no ha sido enviado.";
+$FileUploadSucces = "El archivo ha sido enviado.";
+$MailingNonMailingError = "Un correo no puede ser sobrescrito por un no-correo y viceversa";
+$MailingSelectNoOther = "El correo no se puede combinar con otros recipientes";
+$MailingJustUploadSelectNoOther = "El envío a mi mismo no se puede combinar con otros recipientes";
+$NoFilesHere = "Aquí no hay archivos";
+?>
