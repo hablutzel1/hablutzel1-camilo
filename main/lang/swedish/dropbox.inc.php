1e18a847374221c9495a1eda849fce3c6215c47a
Ivan Tcholakov <Ivan Tcholakov@localhost>
2010-03-04 19:59:52 +0200

    Feature #272 - "Configure homepage": Reworks for managing encodings of the included html-snippets.

---
+++ main/lang/swedish/dropbox.inc.php
@@ -0,0 +1,50 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$ReceivedFiles = "Mottagna filer";
+$SentFiles = "Skickade filer";
+$ReceivedTitle = "Titel";
+$SentTitle = "Titel";
+$Authors = "Författare";
+$Size = "Storlek";
+$kB = "kB";
+$NoSentFilesHere = "Det finns inga <STRONG>skickade</STRONG> filer att visa här.";
+$NoReceivedFilesHere = "Det finns inga <STRONG>mottagna</STRONG> filer att visa här.";
+$UploadNewFile = "Skicka en ny fil";
+$AreYouSureToDelete = "Är du säker på att du vill radera";
+$Feedback = "Återkoppling";
+$CloseFeedback = "Stäng återkoppling";
+$AddNewFeedback = "Lägg till återkoppling";
+$DropboxFeedbackStored = "Återkoppling sparades";
+$AllUsersHaveDeletedTheFileAndWillNotSeeFeedback = "Alla användare har tagit bort filen så ingen kommer att se återkopplingen du skrev.";
+$FeedbackError = "Fel vid återkoppling";
+$PleaseTypeText = "Skriv något.";
+$YouAreNotAllowedToDownloadThisFile = "Du har inte behörighet att ladda ner denna fil.";
+$CheckAtLeastOneFile = "Kontrollera minst en fil.";
+$ReceivedFileDeleted = "Den mottagna filen är raderad";
+$SentFileDeleted = "Den skickade filen är raderad.";
+$FilesMoved = "Valda filer har flyttats.";
+$ReceivedFileMoved = "Mottagna filer har flyttats.";
+$SentFileMoved = "Skickade filer har flyttats";
+$NotMovedError = "Filen/filerna kan inte flyttas.";
+$AddNewCategory = "Lägg till en ny kategori";
+$EditCategory = "Ändra denna kategori";
+$CategoryName = "Kategorinamn";
+$ErrorPleaseGiveCategoryName = "Var vänlig ange kategorinamn";
+$CategoryAlreadyExistsEditIt = "Kategorinamnet finns redan, välj ett annat";
+$CurrentlySeeing = "Du ser redan kategorin";
+$CategoryStored = "Kategorin är sparad";
+$CategoryModified = "Kategorin är ändrad";
+$CategoryDeleted = "Kategorin är raderad";
+$AuthorFieldCannotBeEmpty = "Ägarnamn måste anges";
+$YouMustSelectAtLeastOneDestinee = "Du måste välja minst en mottagare";
+$InvalidUserDetected = "Ogiltig användare funnen";
+$InvalidGroupDetected = "Ogiltig grupp funnen";
+$DropboxFileTooBig = "Filen är för stor för inlämningsmappen";
+$TheFileIsNotUploaded = "Filen är inte uppladdad.";
+$FileUploadSucces = "Filuppladdningen lyckades.";
+$NoFilesHere = "Inga filer här";
+$NewDropboxFileUploaded = "Ny fil i inlämningsmappen";
+$NewDropboxFileUploadedContent = "En ny fil har lämnats i inlämningsmappen för din kurs";
+?>
---
+++ main/lang/turkce/dropbox.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langAddEdit = "Ekle / Düzenle";
+$ErrorNoFilesInFolder = "Dizin boş";
+$AddComment = "Geri beslemeyi kaydet";
+$SentCatgoryDeleted = "Dizin silindi";
+$ReceivedCatgoryDeleted = "Dizin silindi";
+?>
