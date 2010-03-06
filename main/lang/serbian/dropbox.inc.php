313fdcc01e0178bb186dd027bdb046b49af5a685
Ivan Tcholakov <Ivan Tcholakov@localhost>
2010-03-06 02:04:59 +0200

    A technical correction for the file access_url_edit.php

---
+++ main/lang/serbian/dropbox.inc.php
@@ -0,0 +1,32 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$ReceivedFiles = "Primljeni fajlovi";
+$SentFiles = "Poslati fajlovi";
+$ReceivedTitle = "Naslov";
+$SentTitle = "Naslov";
+$Authors = "Autori";
+$Size = "Veličina";
+$LastResent = "Poslednji put poslato";
+$kB = "kB";
+$Root = "Osnovni";
+$NoSentFilesHere = "Ne postoje <b>poslate</b> datoteke za prikaz.";
+$NoReceivedFilesHere = "Ne postoje <b>primljene</b> datoteke za prikaz.";
+$UploadNewFile = "Pošalji novu datoteku";
+$AreYouSureToDelete = "Jeste li sigurni da želite da obrišete";
+$Feedback = "Povratna informacija";
+$CloseFeedback = "Zatvori povratnu informaciju";
+$AddNewFeedback = "Dodaj povratnu informaciju";
+$DropboxFeedbackStored = "Poruka povratne informacije je snimljena";
+$AllUsersHaveDeletedTheFileAndWillNotSeeFeedback = "Svi korisnici su obrisali datoteku zato niko neće videti povratnu informaciju koju dodajete.";
+$PleaseTypeText = "Molim unesite neki tekst.";
+$DropboxFileTooBig = "Datoteka je prevelika.";
+$TheFileIsNotUploaded = "Fajl nije uspešno snimljen na server";
+$FileUploadSucces = "Fajl je uspešno snimljen na server";
+$NoFilesHere = "Ne postoje fajlovi";
+$langAddEdit = "Dodaj / Ažuriraj";
+$AddComment = "Snimi povratnu informaciju";
+$SentCatgoryDeleted = "Direktorijum je obrisan";
+$ReceivedCatgoryDeleted = "Direktorijum je obrisan";
+?>
