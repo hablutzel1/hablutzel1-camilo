a30bfd657bc8564b567d8a888dbf9007fef7eb02
Ivan Tcholakov <Ivan Tcholakov@localhost>
2010-03-10 13:53:24 +0200

    Feature #272 - The internationalization library: Making the date/time formatting functions more convenient for use.

---
+++ main/lang/finnish/resourcelinker.inc.php
@@ -0,0 +1,50 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$ResourceAdded = "Liitteet on lisätty. Voit nyt palata oppimispolkuun tai lisätä uusia liitteitä.";
+$LearningPath = "Learning Path";
+$LevelUp = "level up";
+$AddIt = "Add it";
+$MainCategory = "main category";
+$lang_delete_added_resources = "Delete added resources";
+$AddToLinks = "Add to the course links";
+$DontAdd = "do not add";
+$lang_show_all_added_resources = "Show all added resources";
+$ResourcesAdded = "Added new resources";
+$BackTo = "Back to";
+$ExternalResources = "External resources";
+$CourseResources = "Course resources";
+$ExternalLink = "External link";
+$DropboxAdd = "Add the dropbox page to this chapter.";
+$AddAssignmentPage = "Add the upload assignment page to this chapter.";
+$Exercise = "Test";
+$Link = "Link";
+$AdValvas = "Ad Valvas";
+$Document = "Document";
+$ExportableCourseResources = "Siirrettäviä kurssimateriaaleja";
+$DokeosRelatedCourseMaterial = "Dokeokseen liittyviä kurssimateriaaleja";
+$LinkTarget = "Linkin kohde";
+$SameWindow = "Samassa ikkunassa";
+$NewWindow = "Uudessa ikkunassa";
+$StepDeleted1 = "Tämä";
+$StepDeleted2 = "on poistettu tässä työkalussa.";
+$Modify = "Muokkaa";
+$Chapter = "Kappale";
+$NoAgendaItems = "Kalenterimerkintöjä ei ole";
+$AgendaAdd = "Lisää uusi merkintä";
+$UserGroupFilter = "Suodatin ryhmisä/kättäjissä";
+$AgendaSortChronologicallyUp = "Nouseva";
+$ShowCurrent = "Näytä tämän kuun kalenterimerkinnät";
+$ModifyCalendarItem = "Muokkaa merkintää";
+$ItemTitle = "Merkinnän otsikko";
+$Detail = "Yksityiskohdat";
+$EditSuccess = "Merkintää on muokattu";
+$AddCalendarItem = "Lisää uusi merkintä";
+$AddAnn = "Lisää ilmoitus";
+$ForumAddNewTopic = "Foorumi: lisää uusi aihe";
+$ForumEditTopic = "Foorumi: muokkaa aihetta";
+$ExerciseAnswers = "Tehtävä: Vastaus";
+$ForumReply = "Foorumi: vastaus";
+$AgendaSortChronologicallyDown = "Laskeva";
+?>
