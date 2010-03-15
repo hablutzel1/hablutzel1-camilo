86dd9b47100e53eb26c43afdb7bc847f2ff956be
cvargas <cvargas@localhost>
2010-03-15 11:34:32 -0500

    Create introduction to the course and introduction to the tools for session. CT#750

---
+++ main/lang/latvian/link.inc.php
@@ -0,0 +1,44 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langLinkName = "Hipersaites nosaukums";
+$langLinkAdd = "Pievienot hipersaiti";
+$langLinkAdded = "Hipersaite tika pievienota.";
+$langLinkMod = "Labot hipersaiti";
+$langLinkModded = "Hipersaite tika labota.";
+$langLinkDel = "Dzēst hipersaiti";
+$langLinkDeleted = "Hipersaite tika dzēsta";
+$langLinkDelconfirm = "Vai Jūs vēlaties dzēst";
+$langAllLinksDel = "Dzēst visas hipersaites";
+$langCategoryName = "Nodaļas nosaukums";
+$langCategoryAdd = "Pievienot kategoriju";
+$langCategoryAdded = "Kategorija tika pievienota.";
+$langCategoryModded = "Kategorija tika labota.";
+$langCategoryDel = "Dzēst kategoriju";
+$langCategoryDeleted = "Kategorija un visas tās hipersaites tika izdzēstas.";
+$langCategoryDelconfirm = "Kad tiek dzēsta kategorija, visas hipersaites tajā tiek dzēsti.\nVai Jūs vēlaties dzēst";
+$langAllCategoryDel = "Dzēst visas kategorijas un visas saites";
+$langGiveURL = "Lūdzu, norādiet hipersaites URL";
+$langGiveCategoryName = "Lūdzu, norādiet kategorijas nosaukumu";
+$langNoCategory = "Nav nevienas kategorijas";
+$showall = "Atvērt visas kategorijas";
+$shownone = "AIzvērt visas kategorijas";
+$langListDeleted = "Saraksts tika dzēsts";
+$langAddLink = "Pievienot hipersaiti";
+$langDelList = "Dzēst sarakstu";
+$langModifyLink = "Labot saiti";
+$langCsvImport = "CSV importē";
+$langCsvFileNotFound = "Importējamo CSV failu nebija iespējams atvērt (t.i., tuk";
+$langCsvFileNoSeps = "Importējamam CSV failā kā atdalītājsimbolu jālieto \",\" vai \";\"";
+$langCsvFileNoURL = "Importējamajā CSV failā jābūt vismaz URL un nosaukuma kolonnām";
+$langCsvFileLine1 = "... - line 1 =";
+$langCsvLinesFailed = "rindā(s) netika veikta importa procedūra (nav URL vai nosaukuma)";
+$langCsvLinesOld = "eso";
+$langCsvLinesNew = "jauna(s) hipersaite(s) tika izveidota(s).";
+$langCsvExplain = "Failam jāizskatās";
+$langLinkUpdated = "Hipersaite tika atjaunota";
+$langAll_Link_Deleted = "Hipersaite tika dzēsta";
+$langOnHomepage = "Rādīt hipersaiti sākumlapā";
+$langShowLinkOnHomepage = "�o hipersaiti rādīt kā ikonu kursa sākumlapā";
+?>
