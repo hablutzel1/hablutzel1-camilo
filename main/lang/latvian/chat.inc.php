8ee0fae9fedd0d53e2bbd059412d185cd36a170e
cvargas <cvargas@localhost>
2010-03-17 16:43:32 -0500

    enable tutors  session, management evaluations CT#601

---
+++ main/lang/latvian/chat.inc.php
@@ -0,0 +1,39 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langOnlineConference = "Konference";
+$langWash = "Dzēst";
+$langReset = "Atsākt";
+$langSave = "Saglabāt";
+$langRefresh = "Pārlādēt";
+$langIsNowInYourDocDir = "�obrīd ir Jūsu Dokumentu rīkos. <br /><B>�is fails ir redzams</B>";
+$langCopyFailed = "Drukā";
+$langTypeMessage = "Rakstiet savu ziņu!";
+$langConfirmReset = "Vai tie";
+$langHasResetChat = "atsāka no jauna pļāpā";
+$langNoOnlineConference = "�obrīd nav nevienas konferences ...";
+$langMediaFile = "Tie";
+$langContentFile = "Prezentācija";
+$langListOfParticipants = "Dalībnieku saraksts";
+$langYourPicture = "Jūsu attēls";
+$langOnlineDescription = "Konfidences detaļas";
+$langOnlyCheckForImportantQuestion = "Lūdzu, atzīmē";
+$langQuestion = "jautājums";
+$langClearList = "Dzēst sarakstu";
+$langWhiteBoard = "Baltā tāfele";
+$langTextEditorDefault = "<h2>Teksta redaktors</h2>Izgriez un ielīmē";
+$langStreaming = "Straumīte";
+$langStreamURL = "Straumītes URL";
+$langStreamType = "Straumītes tips";
+$langLinkName = "Hipersaites nosaukums";
+$langLinkURL = "Hipersaites URL";
+$langWelcomeToOnlineConf = "Laipni lūdzam <b>Tie";
+$langNoLinkAvailable = "Nav neviena saite pieejama";
+$langChat_reset_by = "pļāpā";
+$OrFile = "Vai fails";
+$langCallSent = "Uzaicinājums pievienoties pļāpātuvei tika nosūtīts. Tagad jāuzgaida līdz aicinātais piekritīs piedalīties sarunā.";
+$langChatDenied = "Uzaicinājums tika noraidīts.";
+$Send = "Sūtīt";
+$Connected = "Savienots";
+?>
