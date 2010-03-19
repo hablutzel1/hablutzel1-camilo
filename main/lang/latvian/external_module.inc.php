07910c63cd1539ad6f7e6a82b4bc59fc4f673f40
Julio Montoya <Julio Montoya@localhost>
2010-03-19 11:07:26 -0500

    Sql query improved

---
+++ main/lang/latvian/external_module.inc.php
@@ -0,0 +1,22 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langLinkSite = "Pievienot hipersaiti sākumlapā";
+$langSubTitle = "Pievieno norādi uz kādu konkrētu lapu no Jūsu kursa apraksta lapas. Lai to izdarītu, nokopējiet URL pārlūkprogrammas adreses laukā un ievietojiet to \"Hipersaite\" lauciņā zemāk.";
+$langAddPage = "Pievienot lapu";
+$langSendPage = "Lapu, kas jāaug";
+$langCouldNot = "Failu nebija iespējams aug";
+$langTooBig = "Jūs neizvēlējāties nevienu failu, ko nosūtīt, vai arī tas ir pārāk liela izmēra";
+$langExplanation = "Tiklīdz nospiedīsiet OK, tiks izveidota kursa mājas lapa ar Forumu, Plānotāju, Dokumentu rīku, u.t.t. Piesakoties sitēmā ar savu lietotājvārdu un paroli, Tu varēsi veidot un labot";
+$langPgTitle = "Lapas virsraksts";
+$langNoLinkURL = "Lūdzu, norādiet saites URL";
+$langLinkTarget = "Saites galamērķis";
+$langSameWindow = "Atveras tanī pa";
+$langNewWindow = "Atveras jaunā logā";
+$langAdded = "Pievienots";
+$langAddLink = "Pievienot hipersaiti";
+$langNoLinkName = "Lūdzu, ievadiet saites nosaukumu";
+$langEditLink = "Labot Kursa apraksta lapas saiti";
+$langChangePress = "Veiciet izmaiņas un spiediet \"Darīts\"";
+?>
