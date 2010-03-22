89c627faa1c96e4006d55e4573cbdaca40424dc2
cvargas <cvargas@localhost>
2010-03-22 12:18:28 -0500

    DLTT Import

---
+++ main/lang/latvian/notification.inc.php
@@ -0,0 +1,19 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$lang_new_item = "jauns objekts pievienots";
+$lang_title_notification = "Kop";
+$lang_update_agenda = "eso";
+$lang_new_agenda = "jauns plānotāja objekts pievienots";
+$lang_update_announcements = "eso";
+$lang_new_announcements = "jauns paziņojuma objekts pievienots";
+$lang_new_document = "jauns dokuments(i) pievienots(i)";
+$lang_new_exercise = "jauns uzdevums pievienots";
+$lang_update_link = "eso";
+$lang_new_link = "jauna hipersaite pievienota";
+$lang_new_forum_topic = "jauna tēma pievienota";
+$lang_new_groupforum_topic = "jauna tēma pievienota grupas diskusijai";
+$lang_new_dropbox_file = "jauns fails saņemts";
+$lang_update_dropbox_file = "fails Jūsu apmaiņas kastē tika atjaunots";
+?>
