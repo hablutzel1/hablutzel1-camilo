7d61696168f7ef026d410c02a8e703aef042679e
Julio Montoya <Julio Montoya@localhost>
2010-04-07 18:00:59 -0500

    Coursecopy tool: Changing learnpath class to LearnpathCourseCopy to avoid redeclaration fatal error see CT#886

---
+++ main/lang/simpl_chinese/wiki.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$Print = "打印";
+$Version = "版本";
+$EditPage = "编辑";
+$MostActiveUsers = "更多在线用户";
+$Restore = "恢复";
+$CurrentVersion = "当前版本";
+$LastVersion = "最后版本";
+$PageRestored = "页面已恢复。点击浏览";
+$RestoredFromVersion = "恢复自版本";
+?>
