8ce6ca738cd5106af74e3d41a9ec9302eeaffa4c
ywarnier <ywarnier@localhost>
2010-04-07 17:43:47 -0500

    languages import

---
+++ main/inc/reduced_header.inc.php
@@ -0,0 +1,176 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/**
+ *	This script displays the Dokeos header up to the </head> tag
+ *   IT IS A COPY OF header.inc.php EXCEPT that it doesn't start the body
+ *   output.
+ *
+ *	@package chamilo.include
+ */
+
+/*  HEADERS SECTION */
+
+/*
+ * HTTP HEADER
+ */
+
+header('Content-Type: text/html; charset='.api_get_system_encoding());
+
+if (isset($httpHeadXtra) && $httpHeadXtra) {
+	foreach ($httpHeadXtra as & $thisHttpHead) {
+		header($thisHttpHead);
+	}
+}
+
+// Get language iso-code for this page - ignore errors
+$document_language = api_get_language_isocode();
+
+/*
+ * HTML HEADER
+ */
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $document_language; ?>" lang="<?php echo $document_language; ?>">
+<head>
+<title>
+<?php
+if (!empty($nameTools)) {
+	echo $nameTools.' - ';
+}
+
+if (!empty($_course['official_code'])) {
+	echo $_course['official_code'].' - ';
+}
+
+echo api_get_setting('siteName');
+?>
+</title>
+
+<?php
+
+/*
+ * Choose CSS style platform's, user's, course's, or Learning path CSS
+ */
+
+$platform_theme = api_get_setting('stylesheets'); 	// plataform's css
+$my_style = $platform_theme;
+if (api_get_setting('user_selected_theme') == 'true') {
+	$useri = api_get_user_info();
+	$user_theme = $useri['theme'];
+	if (!empty($user_theme) && $user_theme != $my_style) {
+		$my_style = $user_theme;					// user's css
+	}
+}
+$mycourseid = api_get_course_id();
+if (!empty($mycourseid) && $mycourseid != -1) {
+	if (api_get_setting('allow_course_theme') == 'true') {
+		$mycoursetheme=api_get_course_setting('course_theme');
+		if (!empty($mycoursetheme) && $mycoursetheme != -1) {
+			if (!empty($mycoursetheme) && $mycoursetheme != $my_style) {
+				$my_style = $mycoursetheme;		// course's css
+			}
+		}
+
+		$mycourselptheme=api_get_course_setting('allow_learning_path_theme');
+		if (!empty($mycourselptheme) && $mycourselptheme != -1 && $mycourselptheme == 1) {
+			global $lp_theme_css; //  it comes from the lp_controller.php
+			global $lp_theme_config; // it comes from the lp_controller.php
+
+			if (!empty($lp_theme_css))
+				{
+					$theme=$lp_theme_css;
+					if (!empty($theme) && $theme != $my_style) {
+						$my_style = $theme;	 // LP's css
+					}
+				}
+
+		}
+	}
+}
+
+if (!empty($lp_theme_log)) {
+	$my_style = $platform_theme;
+}
+
+// Sets the css reference it is call from lp_nav.php, lp_toc.php, lp_message, lp_log.php
+if (!empty($scorm_css_header)) {
+	if (!empty($my_style)) {
+		$scorm_css = api_get_path(WEB_CSS_PATH).$my_style.'/scorm.css';
+		$scormfs_css = api_get_path(WEB_CSS_PATH).$my_style.'/scormfs.css';
+	} else {
+		$scorm_css = 'scorm.css';
+		$scormfs_css = 'scormfs.css';
+	}
+
+	if (!empty($display_mode) && $display_mode == 'fullscreen') {
+		$htmlHeadXtra[] = '<style type="text/css" media="screen, projection">
+							/*<![CDATA[*/
+							@import "'.$scormfs_css.'";
+							/*]]>*/
+							</style>';
+	} else {
+		$htmlHeadXtra[] = '<style type="text/css" media="screen, projection">
+							/*<![CDATA[*/
+							@import "'.$scorm_css.'";
+							/*]]>*/
+							</style>';
+	}
+}
+
+
+if ($my_style != '') {
+?>
+<style type="text/css" media="screen, projection">
+/*<![CDATA[*/
+@import "<?php echo api_get_path(WEB_CSS_PATH), $my_style; ?>/default.css";
+/*]]>*/
+</style>
+<?php
+}
+?>
+
+<link rel="top" href="<?php echo api_get_path(WEB_PATH); ?>index.php" title="" />
+<link rel="courses" href="<?php echo api_get_path(WEB_CODE_PATH) ?>auth/courses.php" title="<?php echo api_htmlentities(get_lang('OtherCourses'),ENT_QUOTES,$charset); ?>" />
+<link rel="profil" href="<?php echo api_get_path(WEB_CODE_PATH) ?>auth/profile.php" title="<?php echo api_htmlentities(get_lang('ModifyProfile'),ENT_QUOTES,$charset); ?>" />
+<link href="http://www.chamilo.org/documentation.php" rel="Help" />
+<link href="http://www.chamilo.org/team.php" rel="Author" />
+<link href="http://www.chamilo.org" rel="Copyright" />
+<link rel="shortcut icon" href="<?php echo api_get_path(WEB_PATH); ?>favicon.ico" type="image/x-icon" />
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo api_get_system_encoding(); ?>" />
+
+<script type="text/javascript">
+//<![CDATA[
+// This is a patch for the "__flash__removeCallback" bug, see FS#4378.
+if ( ( navigator.userAgent.toLowerCase().indexOf('msie') != -1 ) && ( navigator.userAgent.toLowerCase().indexOf( 'opera' ) == -1 ) )
+{
+	window.attachEvent( 'onunload', function()
+		{
+			window['__flash__removeCallback'] = function ( instance, name )
+			{
+				try
+				{
+					if ( instance )
+					{
+						instance[name] = null ;
+					}
+				}
+				catch ( flashEx )
+				{
+
+				}
+			} ;
+		}
+	) ;
+}
+//]]>
+</script>
+
+<?php
+if (isset($htmlHeadXtra) && $htmlHeadXtra) {
+	foreach ($htmlHeadXtra as & $this_html_head) {
+		echo $this_html_head;
+	}
+}
+?>
+</head>
---
+++ main/lang/simpl_chinese/userInfo.inc.php
@@ -0,0 +1,34 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langLineNumber = "行数";
+$langLine = "行";
+$langLines = "行";
+$langLineOrLines = "行";
+$langMoveUp = "上移";
+$langMoveDown = "下移";
+$langAddNewHeading = "添加新标题";
+$langCourseAdministratorOnly = "仅教师使用";
+$langDefineHeadings = "定义标题";
+$langBackToUsersList = "返回列表";
+$langTracking = "学习进度";
+$langCourseManager = "课程管理员";
+$langModRight = "更改权限";
+$langNoAdmin = "从现在开始<B>无权</B>查看本页";
+$langAllAdmin = "从现在开始<b>有权</b>查看此页";
+$langModRole = "改变角色";
+$langRole = "职务";
+$langIsNow = "是从现在开始的";
+$langInC = "在本课程中";
+$langFilled = "您有些选项没有填写。";
+$langUserNo = "您选择的用户名";
+$langTaken = "已被占用，请选择其他的用户名。";
+$langTutor = "教师";
+$langUnreg = "注销";
+$langGroupUserManagement = "小组管理";
+$langUnregister = "注销";
+$langAddAUser = "添加用户";
+$UsersUnsubscribed = "用户已经注销";
+$ThisStudentIsSubscribeThroughASession = "学生已经从本课程注销，你不能编辑此类信息";
+?>
