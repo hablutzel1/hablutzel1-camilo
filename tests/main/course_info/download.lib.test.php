899463aa6cd927a410af60de1c2b279b8fb473d9
Guillaume Viguier <guillaume.viguier@beeznest.com>
2010-06-15 17:33:58 -0500

    Adding WSListCourses as a SOAP service - BT#1111

---
+++ tests/main/course_info/download.lib.test.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once api_get_path(SYS_CODE_PATH).'course_info/download.lib.php';
+
+
+class TestCreateBackupIsAdmin extends UnitTestCase {
+
+   public function Testcreate_backup_is_admin() 
+   {
+   		$_GET = array('archive' => 'index.html');
+   		
+          $resTrue =  create_backup_is_admin(true);
+          $this->assertTrue(is_bool($resTrue));
+          $resFalse = create_backup_is_admin(false);
+          $this->assertFalse($resFalse);
+   		  $this->assertEqual($resTrue , $resFalse);
+   		  //var_dump($resTrue, $resFalse);
+   		  
+   }
+}
+
+?>
