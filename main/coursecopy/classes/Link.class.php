2e394f6d7f89f813f8e795d39f333caf9d76aae6
Ivan Tcholakov <Ivan Tcholakov@localhost>
2010-06-27 03:26:16 +0300

    Feature #744 - Documents tool, slideshow mode: The navigation buttons ("First", "Previous", "Next", "Last") have been moved on the action bar.

---
+++ main/coursecopy/classes/Link.class.php
@@ -0,0 +1,57 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+require_once 'Resource.class.php';
+
+/**
+ * A WWW-link from the Links-module in a Dokeos-course.
+ * @author Bart Mollet <bart.mollet@hogent.be>
+ */
+class Link extends Resource
+{
+	/**
+	 * The title
+	 */
+	var $title;
+	/**
+	 * The URL
+	 */
+	var $url;
+	/**
+	 * The description
+	 */
+	var $description;
+	/**
+	 * Id of this links category
+	 */
+	var $category_id;
+	/**
+	 * Display link on course homepage
+	 */
+	var $on_homepage;
+	/**
+	 * Create a new Link
+	 * @param int $id The id of this link in the Dokeos-course
+	 * @param string $title
+	 * @param string $url
+	 * @param string $description
+	 */
+	function Link($id,$title,$url,$description,$category_id,$on_homepage)
+	{
+		parent::Resource($id,RESOURCE_LINK);
+		$this->title = $title;
+		$this->url = $url;
+		$this->description = $description;
+		$this->category_id = $category_id;
+		$this->on_homepage = $on_homepage;
+	}
+	/**
+	 * Show this resource
+	 */
+	function show()
+	{
+		parent::show();
+		echo $this->title.' ('.$this->url.')';
+	}
+}
+?>
