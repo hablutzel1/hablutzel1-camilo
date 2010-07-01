bfc9800142b5ee48a8968cf883376ebc3f1914f4
Julio Montoya <gugli100@gmail.com>
2010-07-01 12:33:41 +0200

    Replacing security:remove_XXS with intval to reduce htmlpurifier calls see #1450

---
+++ main/lang/latvian/announcements.inc.php
@@ -0,0 +1,52 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$Announcement = "Paziņojumi";
+$langAnnEmpty = "Paziņojumu saraksts tika izdzēsts";
+$AnnouncementModified = "Paziņojums tika labots";
+$AnnouncementAdded = "Paziņojums tika ievietots";
+$AnnouncementDeleted = "Paziņojums tika izdzēsts";
+$AnnouncementPublishedOn = "Ievietots";
+$AddAnnouncement = "Ievietot paziņojumu";
+$langContent = "Saturs";
+$AnnouncementDeleteAll = "Dzēst paziņojumu sarakstu";
+$professorMessage = "Ziņa no docētāja";
+$langEmailSent = "un nosūtīts e-pasts reģistrētajiem studentiem";
+$EmailOption = "Nosūtīt";
+$langOn = "Ieslēgts";
+$langRegUser = "lapas reģistrētie lietotāji";
+$langUnvalid = "ir nepareizas vai vispar nav e-pasta adreses";
+$langModifAnn = "Labot";
+$langModify = "labot";
+$langDelete = "dzēst";
+$langTitle = "Virsraksts";
+$langHelp = "palīdzība";
+$langOk = "Darīts!";
+$langAddIntro = "Pievienot iepazī";
+$langBackList = "Atpakaļ uz sarakstu";
+$langSelMess = "Brīdinājumi da˛iem lietotājiem";
+$EmailTitle = "Tēma";
+$langUserlist = "Grupu un lietotāju saraksts";
+$langSelectedUsers = "Atzīmētie lietotāji";
+$langSubmit = "Sūtīt";
+$langPleaseEnterMessage = "Jums jāuzraksta paziņojuma teksts";
+$langPleaseSelectUsers = "Jums jāizvēlas vismaz da˛i lietotāji";
+$Teachersubject = "Ziņa tika nosūtīts Jūsu studentiem";
+$langMessages = "Ziņas";
+$langMessageToSelectedUsers = "Ziņas atzīmētajiem lietotājiem";
+$langIntroText = "Lai nosūtītu ziņu, iezīmē lietotāju grupas (G priek";
+$langDown = "lejā";
+$langMsgSent = "Ziņa tika nosūtīta atzīmētajiem studentiem";
+$langSelUser = "Iezīmētie lietotāji";
+$langMessageToSelectedGroups = "Ziņa iezīmētajām grupām";
+$langSelectedGroups = "iezīmētās grupas";
+$langMsg = "Ziņas";
+$langYes = "jā";
+$MsgText = "ziņa";
+$AnnouncementDeletedAll = "Visi paziņojumi tika izdzēsti";
+$AnnouncementMoved = "Paziņojums tika pārvietots";
+$NoAnnouncements = "eit nav neviena pazi&#326;ojuma";
+$SelectEverybody = "Iezīmēt visus";
+$SelectedUsersGroups = "Iezīmēt lietotāju grupas";
+?>
