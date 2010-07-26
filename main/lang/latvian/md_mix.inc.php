feefc9f62ce7aebe1fbb2702d2a8cadc9939c399
ywarnier <ywarnier@localhost>
2010-07-26 00:43:26 -0500

    Updated PhoneNumber field in CSV/XML export to match import format (partial CT#1454)

---
+++ main/lang/latvian/md_mix.inc.php
@@ -0,0 +1,19 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langTool = "Dokumenta META dati";
+$langClickKw = "Noklik";
+$langKwHelp = "<br/>Noklik";
+$langAdvanced = "Papildus iespējas";
+$langSearch = "Meklēt";
+$langSearchCrit = "Vienu vārdu katrā rindā!";
+$langNoKeywords = "�im kursam nav atslēgvārdu";
+$langKwCacheProblem = "Atslēgvārdu starpkrātuvi nebija iespējams atvērt";
+$langCourseKwds = "�ajā dokumentā atrodas kursa atslēgvārdi";
+$langKwdsInMD = "Atslēgvārdi, kas tika lietoti MD";
+$langKwdRefs = "atslēgvārdu atsauces";
+$langNonCourseKwds = "Atslēgvārdi, kas neattiecas uz kursu";
+$langKwdsUse = "Kursa atslēgvārdi (bold = netiek izmantoti)";
+$langTotalMDEs = "Kopējais hipersai";
+?>
