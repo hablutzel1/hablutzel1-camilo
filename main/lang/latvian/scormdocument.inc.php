e7855f8b2d5ae4a8e1e4e85f058cd1b3c8a488c5
ywarnier <ywarnier@localhost>
2010-08-15 14:32:41 -0500

    Added default category in admin settings

---
+++ main/lang/latvian/scormdocument.inc.php
@@ -0,0 +1,64 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langChapter = "Nodaļa";
+$langDocumentList = "Visu dokumentu saraksts";
+$langOrganisationList = "Saturs";
+$langEditTOC = "Labot satura koku";
+$langEditDocument = "Labot";
+$langCreateDocument = "Izveidot dokumentu";
+$langMissingImagesDetected = "Konstatēti iztrūksto";
+$langPublish = "Publicēt";
+$langScormcontentstudent = "Pieeja <i>Scorm</i> formāta kursam. Pieejai klik";
+$langScormcontent = "Pieeja <i>Scorm</i>  sistēmai<br><input type=button name=scormbutton value='Sākt darbu !' onclick='openscorm()'>";
+$langDownloadAndZipEnd = "ZIP fails aug";
+$langZipNoPhp = "ZIP failā nedrīkst būt iekļauti .PHP faili";
+$langGroupForumLink = "Grupas diskusijas";
+$langGroupSpace = "Grupas apraksts";
+$langNotScormContent = "�is nav <i>Scorm</i> formāta ZIP fails!";
+$langNoText = "Lūdzu, ievadiet tekstuālo / HTML informāciju";
+$langNoFileName = "Lūdzu, norādiet faila nosaukumu";
+$langMaxFileSize = "Maksimāli pieļaujamais faila izmērs ir";
+$langFileName = "Faila nosaukums";
+$langFileError = "Aug";
+$langDown = "lejā";
+$langViMod = "Pieejamība pārskatīta";
+$langAddComment = "Pievienot/labot komentāru";
+$langImpossible = "Darbība ir neiespējama";
+$langNewDir = "Jaunās direktorijas nosaukums";
+$langTo = "uz";
+$langNoSpace = "Aug";
+$langDownloadEnd = "Aug";
+$langFileExists = "Veicamā darbība ir neiespējama - fails ar tādu nosaukumu jau eksistē.";
+$langDocCopied = "Dokuments nokopēts";
+$langDocDeleted = "Dokuments izdzēsts";
+$langElRen = "Elements pārsaukts";
+$langDirMv = "Elements pārvietots";
+$langComMod = "Komentārs papildināts";
+$langRename = "Pārsaukt";
+$langCopy = "Kopēt kursa saturu";
+$langNameDir = "Jaunās direktorijas nosaukums";
+$langDownloadFile = "Aug";
+$learning_path_access = "noklik";
+$langRoot = "pamatdirektorija";
+$langBuilder = "Mācību programmas veidotājs";
+$lang_edit_learnpath = "labot mācību plānu";
+$lang_add_title_and_desc = "Labot virsrakstu un aprakstu";
+$lang_add_learnpath = "Pievienojiet jaunu mācību plānu";
+$lang_learnpath_added = "Jaunais mācību plāns tika veiksmīgi izveidots. Jūs tagad varat pievienot moduļus, klik";
+$lang_delete_learnpath = "dzēst mācību plānu";
+$lang_learnpath_edited = "mācību plāns tika labots";
+$lang_learnpath_deleted = "mācību plāns tika izdzēsts";
+$lang_no_publish = "nepublicēt";
+$lang_publish = "publicēt";
+$lang_no_published = "nav publicēts";
+$lang_published = "publicēts";
+$lang_description = "apraksts";
+$lang_title = "virsraksts";
+$langScormBuilder = "Plāna veidotājs - <i>Scorm</i> formāta kursa veidotājs";
+$langCreateDoc = "Izveidot dokumentu";
+$langOrganiseDocuments = "Izveidot satura koku";
+$langUncompress = "atarhivēt ZIP failu";
+$ExportShort = "Eksportēt (pa fiksam?)";
+?>
