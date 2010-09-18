d628af89e18736067bb110db82df31c9f54fe9a0
herodoto <herodoto@localhost>
2010-09-18 11:24:31 +0200

    Merge

--- main/exercice/exercice_submit.php
+++ main/exercice/exercice_submit.php
@@ -779,7 +779,9 @@ if ($formSent) {
                     //Verify if the current test is fraudulent
                     $current_time = time();
 
-                    if (isset($_SESSION['expired_time'][$current_expired_time_key]) && $exercise_row['expired_time'][$exerciseId] != 0) {
+					//The $exercise_row['expired_time'][$exerciseId] is never set. It means nothing.
+                    //if (isset($_SESSION['expired_time'][$current_expired_time_key]) && $exercise_row['expired_time'][$exerciseId] != 0) {                    
+                    if (isset($_SESSION['expired_time'][$current_expired_time_key])) {
                         $expired_date = $_SESSION['expired_time'][$current_expired_time_key];
                         $expired_time = strtotime($expired_date);
 
@@ -838,7 +840,7 @@ if (!isset ($_SESSION['objExercise']) || $_SESSION['objExercise']->id != $_REQUE
     }
 }
 
-if (!isset ($objExcercise) && isset ($_SESSION['objExercise'])) {
+if (!isset ($objExercise) && isset ($_SESSION['objExercise'])) {
     $objExercise = $_SESSION['objExercise'];
 }
 if (!is_object($objExercise)) {
@@ -890,7 +892,7 @@ if (!isset ($_SESSION['questionList'])) {
         echo str_repeat('&nbsp;', 0) . '$_SESSION[questionList] was unset - set now - end' . "<br />\n";
     }
 }
-if (!isset ($objExcercise) && isset ($_SESSION['objExercise'])) {
+if (!isset ($objExercise) && isset ($_SESSION['objExercise'])) {
     $questionList = $_SESSION['questionList'];
 }
 
