ce2e56dfd5d93ad4f2b0dba782e28039675a396d
ywarnier <ywarnier@localhost>
2011-02-21 14:58:43 -0500

    Added JKB in credits

---
+++ main/lang/latvian/resourcelinker.inc.php
@@ -0,0 +1,58 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$ResourceAdded = "Resurss pievienots. Jūs varat atgriezties pie mācību plāna vai pievienot vēl kādu resursu.";
+$LearningPath = "Mācību gaita";
+$LevelUp = "līmeni aug";
+$AddIt = "Pievienot to";
+$MainCategory = "galvenā kategorija";
+$lang_delete_added_resources = "Dzēst pievienotos resursus";
+$AddToLinks = "Pievienot kursa hipersaitēm";
+$DontAdd = "nepievienot";
+$lang_show_all_added_resources = "Skatīt visus pievienotos resursus";
+$ResourcesAdded = "Pievienotie resursi";
+$BackTo = "Atpakaļ pie";
+$ExternalResources = "Ārēji resursi";
+$CourseResources = "Kursa resursi";
+$ExternalLink = "Ārēja hipersaite";
+$DropboxAdd = "Pievienot \"Apmaiņas kasti\"";
+$AddAssignmentPage = "Pievienot \"Studentu publikāciju\" lapu";
+$Exercise = "Kontroldarbi";
+$Link = "Hipersaite";
+$AdValvas = "Paziņojumi";
+$Document = "Dokuments";
+$ShowDelete = "Rādīt / Dzēst";
+$IntroductionText = "Ievadteksts";
+$CourseDescription = "Kursa apraksts";
+$Groups = "Grupas";
+$Users = "Lietotāji";
+$IntroductionTextAdd = "Pievienot lapu, kas satur ievadtekstu";
+$CourseDescriptionAdd = "Pievienot lapu, kas satur kursa aprakstu";
+$GroupsAdd = "Pievienot Grupu lapu";
+$UsersAdd = "Pievienot Lietotāju lapu";
+$ExportableCourseResources = "Eksportējami kursa resursi";
+$DokeosRelatedCourseMaterial = "Ar DOKEOS saistīti kursa resursi";
+$LinkTarget = "Hipersaitei jāatveras";
+$SameWindow = "Tajā pa";
+$NewWindow = "Jaunā logā";
+$StepDeleted1 = "is";
+$StepDeleted2 = "objekts tikai tajā rīkā izdzēsts.";
+$Modify = "pārveidot";
+$Chapter = "Sadaļa";
+$NoAgendaItems = "Nav neviena Pl&#257;not&#257;ja ieraksta";
+$AgendaAdd = "Ierakstīt jaunu notikumu plānotājā";
+$UserGroupFilter = "Grupu un lietotāju filtrs";
+$AgendaSortChronologicallyUp = "Sakārtot Plānotāju hronoloģiski (aug";
+$ShowCurrent = "Parādīt";
+$ModifyCalendarItem = "Labot plānotāja notikuma ierakstu";
+$ItemTitle = "Notikuma virsraksts";
+$Detail = "Sīkāk";
+$EditSuccess = "Notikums plānotājā tika izlabots";
+$AddCalendarItem = "Ierakstīt jaunu notikumu plānotājā";
+$AddAnn = "Pievienot paziņojumu";
+$ForumAddNewTopic = "Diskusija: pievienot jaunu tēmu";
+$ForumEditTopic = "Diskusija: labot tēmu";
+$ExerciseAnswers = "Uzdevums: atbildes";
+$ForumReply = "Diskusija: atbildēt";
+?>
