0d98fece67cdd2bead1e085b1e87d8b1220aa0c3
herodoto <herodoto@localhost>
2011-03-06 22:45:55 +0100

    Feature #2785 tables wiki tool

---
+++ main/lang/latvian/coursebackup.inc.php
@@ -0,0 +1,47 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langSelectOptionForBackup = "Lūdzu, izvēlieties rezerves kopijas izveides veidu.";
+$langLetMeSelectItems = "Ļaujiet man izvēlēties kursa pamatdaļas";
+$langCreateFullBackup = "Izveidot pilnu";
+$langCreateBackup = "izveidot rezerves kopiju";
+$langBackupCreated = "&#299; kursa rezerves kopija tika izveidota. Kopijas faila lejupiel&#257;de s&#257;ksies p&#275;c &#299;sa br&#299;ti&#326;a. Ja lejupiel&#257;de nes&#257;kas autom&#257;tiski, sekojiet ai hipersaitei";
+$langSelectBackupFile = "izvēlieties rezerves kopijas failu";
+$langImportBackup = "Importēt rezerves kopiju";
+$langImportFullBackup = "Importēt visu rezerves kopijas saturu";
+$langImportFinished = "Importē";
+$langEvents = "Notikumi";
+$langAnnouncements = "Paziņojumi";
+$langDocuments = "Dokumenti";
+$langTests = "Kontroldarbi";
+$langLearnpaths = "Mācību plāni";
+$langCopyCourse = "Kopēt kursa saturu";
+$langSelectItemsToCopy = "Iezīmēt kopējamās lietas";
+$langCopyFinished = "Kopē";
+$langFullRecycle = "Veikt plnīgu pārstrādi";
+$langRecycleCourse = "Pārstrādāt kursu";
+$langRecycleFinished = "Pārstrāde ir pabeigta";
+$langRecycleWarning = "Brīdinājums: lietojot";
+$langSameFilename = "Ko darīt ar importētajiem failiem, kam ir tādi pa";
+$langSameFilenameSkip = "Izlaist vienādā nosaukuma failus";
+$langSameFilenameRename = "Pārsaukt failu (t.i. <i>fails.pdf</i> kļūst par <i>fails_1.pdf</i>)";
+$langSameFilenameOverwrite = "Aizstāt failu";
+$langSelectDestinationCourse = "Izvēlieties galamērķa kursu";
+$langFullCopy = "Veikt pilnu kopiju";
+$langCourseDescription = "Kursa apraksts";
+$langNoResourcesToBackup = "Nav resursu, kam veidot rezerves kopiju";
+$langNoResourcesInBackupFile = "Rezerves kopijā nav resursu";
+$langSelectResources = "Izvēlieties resursus";
+$langNoResourcesToRecycles = "Nav resursu, ko pārstrādāt";
+$langIncludeQuestionPool = "Iekļaut jautājumu kopumu";
+$langLocalFile = "fails vietējā datorā";
+$langServerFile = "fails uz servera";
+$langNoBackupsAvailable = "rezerves kopiju nav";
+$langNoDestinationCoursesAvailable = "Norādītais kurss nav pieejams";
+$langBackup = "Veidot rezerves kopiju";
+$langImportBackupInfo = "Rezerves kopijas importē";
+$langCreateBackupInfo = "Izveidot rezerves kopiju";
+$ToolIntro = "Apskatīt rīku izmanto";
+$UploadError = "Aug";
+?>
