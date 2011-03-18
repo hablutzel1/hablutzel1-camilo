13ad49cbd4b1c21889161156fc1ebf7daa28e71e
root <root@p2374607.pubip.serverbeach.com>
2011-03-18 17:28:33 +0000

    Fixed missing change in previous commit 7c513325164939df3e11d40ae4a301c6dd6399f0 about create_js() function in learning paths

---
+++ main/lang/latvian/accessibility.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langClarContent = "<p><b>Pasniedzējs</b></p> <p>Dokeos ir apmācības un zinā";
+$test = "tests";
+$WCAGImage = "Attēls";
+$WCAGLabel = "Attēla apraksts";
+$WCAGLink = "Saite";
+$WCAGLinkLabel = "Saites apraksts";
+$errorNoLabel = "im att&#275;lam nav apraksta.";
+$AllLanguages = "Visas valodas";
+$WCAGEditor = "WCAG redaktors";
+$WCAGGoMenu = "Iet uz izvēli";
+?>
