e81ad96236071cb0bd9c980fcc7c13e7331dc6f5
Julio Montoya <gugli100@gmail.com>
2011-04-12 17:46:43 +0200

    More agenda fixes

---
+++ main/lang/spanish/pedaSuggest.inc.php
@@ -0,0 +1,26 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$GeneralDescription = "Descripción general";
+$GeneralDescriptionQuestions = "¿Cuál es el objetivo de esta unidad didáctica? ¿Tiene requisitos previos? ¿Qué relación tiene con otras?";
+$GeneralDescriptionInformation = "Descripción de la unidad didáctica (número de horas, código, lugar donde se desarrollará, etc.). Profesorado (nombre, despacho, teléfono, correo electrónico, etc.).";
+$Objectives = "Objetivos";
+$ObjectivesInformation = "¿Cuáles son los objetivos de la unidad didáctica (competencias, habilidades, resultados,etc.)?";
+$ObjectivesQuestions = "¿Qué deben saber los alumnos al finalizar la unidad didáctica? ¿ Qué objetivos y actividades se desarrollarán durante la misma?";
+$Topics = "Contenidos";
+$TopicsInformation = "Contenidos de la unidad didáctica. Importancia de cada contenido. Nivel de dificultad. Estructura e interdependencia con otros contenidos.";
+$TopicsQuestions = "¿Cuál será el desarrollo de la unidad didáctica?  ¿Dónde deben prestar una especial atención los alumnos? ¿Qué problemas para la comprensión de ciertos temas pueden surgir? ¿Qué tiempo debe dedicarse a cada apartado de la unidad?";
+$Methodology = "Metodología";
+$MethodologyQuestions = "¿Qué métodos y actividades se desarrollarán para alcanzar los objetivos del curso? ¿Qué técnicas y estrategias se desarrollarán para conseguir el aprendizaje de la unidad didáctica? ¿Cuál es el calendario para su realización?";
+$MethodologyInformation = "Presentación de las actividades (conferencias, disertaciones, investigaciones en grupo, laboratorios, etc.).";
+$CourseMaterial = "Materiales";
+$CourseMaterialQuestions = "¿Existe un manual, una colección de documentos, una bibliografía, o una lista de enlaces de Internet?";
+$CourseMaterialInformation = "Breve descripción de los materiales a emplear en la unidad didáctica.";
+$HumanAndTechnicalResources = "Recursos humanos y técnicos";
+$HumanAndTechnicalResourcesQuestions = "¿Quiénes son los profesores, tutores, coordinadores, etc.? ¿Cuáles son los recursos materiales: sala de ordenadores, pizarra digital, conexión a Internet, etc.?";
+$HumanAndTechnicalResourcesInformation = "Identifique y describa los recursos humanos y técnicos.";
+$Assessment = "Evaluación";
+$AssessmentQuestions = "¿Cómo serán evaluados los alumnos? ¿Cuáles son los criterios de evaluación? ¿Qué instrumentos de evaluación se utilizarán?";
+$AssessmentInformation = "Criterios de evaluación de las competencias adquiridas.";
+?>
---
+++ main/messages/view_message.php
@@ -0,0 +1,100 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+*	@package chamilo.messages
+*/
+
+$language_file = array('registration','messages','userInfo');
+$cidReset= true;
+require_once '../inc/global.inc.php';
+api_block_anonymous_users();
+if (api_get_setting('allow_message_tool')!='true') {
+	api_not_allowed();
+}
+require_once api_get_path(LIBRARY_PATH).'message.lib.php';
+
+/*		HEADER  */
+
+if ($_REQUEST['f']=='social') {
+	$this_section = SECTION_SOCIAL;
+	$interbreadcrumb[]= array ('url' => api_get_path(WEB_PATH).'main/social/home.php','name' => get_lang('Social'));
+	$interbreadcrumb[]= array ('url' => 'inbox.php?f=social','name' => get_lang('Inbox'));	
+} else {
+	$this_section = SECTION_MYPROFILE;
+	$interbreadcrumb[]= array ('url' => '#','name' => get_lang('Profile'));
+	$interbreadcrumb[]= array ('url' => 'inbox.php','name' => get_lang('Inbox'));
+}
+$interbreadcrumb[]= array ('url' => '#','name' => get_lang('View'));
+Display::display_header('');
+
+if ($_GET['f']=='social') {	
+	$social_parameter = '?f=social';
+} else {
+	if (api_get_setting('extended_profile') == 'true') {
+		echo '<div class="actions">';
+		
+		if (api_get_setting('allow_social_tool') == 'true' && api_get_setting('allow_message_tool') == 'true') {
+			echo '<a href="'.api_get_path(WEB_PATH).'main/social/profile.php">'.Display::return_icon('shared_profile.png', get_lang('ViewSharedProfile')).'&nbsp;'.get_lang('ViewSharedProfile').'</a>';
+		}
+		if (api_get_setting('allow_message_tool') == 'true') {
+			echo '<a href="'.api_get_path(WEB_PATH).'main/messages/inbox.php">'.Display::return_icon('inbox.png').' '.get_lang('Messages').'</a>';
+		}	
+		echo '<a href="'.api_get_path(WEB_PATH).'main/auth/profile.php?type=reduced">'.Display::return_icon('edit.gif', get_lang('EditNormalProfile')).'&nbsp;'.get_lang('EditNormalProfile').'</a>';
+		
+		echo '</div>';
+	}
+}
+
+echo '<div id="social-content">';	
+	if (empty($_GET['id'])) {
+		$id_message = $_GET['id_send'];
+		$source = 'outbox';
+		$show_menu = 'messages_outbox';
+	} else {
+		$id_message = $_GET['id'];
+		$source = 'inbox';
+		$show_menu = 'messages_inbox';
+	}
+	$id_content_right = '';
+	//LEFT COLUMN
+	if (api_get_setting('allow_social_tool') != 'true') { 
+		$id_content_right = 'inbox';
+		echo '<div id="inbox-menu" class="actions">';
+		echo '<ul>';
+			echo '<li><a href="'.api_get_path(WEB_PATH).'main/messages/new_message.php">'.Display::return_icon('message_new.png',get_lang('ComposeMessage')).get_lang('ComposeMessage').'</a>'.'</li>';
+			echo '<li><a href="'.api_get_path(WEB_PATH).'main/messages/inbox.php">'.Display::return_icon('inbox.png',get_lang('Inbox')).get_lang('Inbox').'</a>'.'</li>';			
+			echo '<li><a href="'.api_get_path(WEB_PATH).'main/messages/outbox.php">'.Display::return_icon('outbox.png',get_lang('Outbox')).get_lang('Outbox').'</a>'.'</li>';
+		echo '</ul>';
+		echo '</div>';
+	} else {
+		require_once api_get_path(LIBRARY_PATH).'social.lib.php';
+		$id_content_right = 'social-content-right';
+		echo '<div id="social-content-left">';	
+			//this include the social menu div			
+			SocialManager::show_social_menu($show_menu);
+		echo '</div>';				
+	}
+
+	echo '<div id="'.$id_content_right.'">';
+	if (api_get_setting('allow_social_tool') == 'true'){
+		echo '<div class="social-box-container2">';				
+		//echo '<div>'.Display::return_icon('content-post-group1.jpg',get_lang('View')).'</div>';
+		//echo '<div id="div_content_table" class="social-box-content2">';
+	}	
+		//MAIN CONTENT
+		$message = MessageManager::show_message_box($id_message,$source);
+		
+		if (!empty($message)) {
+			echo $message;
+		} else {
+			api_not_allowed();
+		}
+	if (api_get_setting('allow_social_tool') == 'true'){
+		echo '</div>';
+		//echo '</div>';
+	}
+	echo '</div>';
+echo '</div>';
+
+/*		FOOTER */
+Display::display_footer();
