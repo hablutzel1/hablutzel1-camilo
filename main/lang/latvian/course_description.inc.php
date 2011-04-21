730b6f6313afab965b3d2adb7eab577e59da32e2
ywarnier <ywarnier@beeznest.org>
2011-04-21 11:24:55 -0500

    Fixed undefined variable in web service call

---
+++ main/lang/latvian/course_description.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langCourseProgram = "Mācību kursa satura un mērķu apraksts";
+$langThisCourseDescriptionIsEmpty = "<b>Izveidojiet šeit, īsu kursa satura aprakstu !</b><br><b>Kursants, aktivizējot INFO (i) ikonu pie kursa nosaukuma. varēs iepazīties ar to, pirms tas ir pieteicies kursam.</b><br><br><br> Savukārt, aizpildot augstāk novietotās sadaļas - Informējiet Kursantus par :<br> -  Kursa mācību mērķiem, <br> - Paredzamajiem testiem,<br> -  Iesaistītajiem mācību spēkiem un citiem resursiem.<br><br><b>Tas palīdzēs Kursantiem vieglāk izprast Kursa saturu un sasniegt vēlamo rezultātu !</b>";
+$langEditCourseProgram = "Izveidot un labot kursa aprakstu";
+$QuestionPlan = "Konsultācijas jautājums kursa veidotājam";
+$langInfo2Say = "Informācija, kas sniedzama lietotājiem";
+$langOuAutreTitre = "Virsraksts";
+$langNewBloc = "Pievienot papildus informatīvo bloku";
+$langAddCat = "Pievienot nodaļai";
+$langAdd = "Pievienot";
+$langValid = "Derīgs";
+$langBackAndForget = "Atpakaļ un dzēst";
+$CourseDescriptionUpdated = "Kursa apraksts papildināts";
+$CourseDescriptionDeleted = "Kursa apraksts izdzēsts";
+$CourseDescriptionIntro = "Lai veidotu kursa aprakstu, klikojiet uz nosaukuma  un aizpildiet lauciņu.<br><br> Spiediet OK, lai turpinātu";
+$langSaveDescription = "Saglabāt aprakstu";
+?>
