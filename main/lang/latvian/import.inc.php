b6a8275821a7d64003d3ea1ba3b295ca8591f42b
jkbockstael <jeankarim@cblue.be>
2011-05-17 21:41:45 +0200

    Storage API : Now using database constants instead of hardcoded table names.

---
+++ main/lang/latvian/import.inc.php
@@ -0,0 +1,16 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langPgTitle = "Lapas virsraksts";
+$langExplanation = "Tiklīdz nospiedīsiet OK, tiks izveidota kursa mājas lapa ar Forumu, Plānotāju, Dokumentu rīku, u.t.t. Piesakoties sitēmā ar savu lietotājvārdu un paroli, Tu varēsi veidot un labot";
+$langTooBig = "Jūs neizvēlējāties nevienu failu, ko nosūtīt, vai arī tas ir pārāk liela izmēra";
+$langCouldNot = "Failu nebija iespējams aug";
+$langNotAllowed = "Nav atļauts";
+$langAddPageToSite = "Pievienot vēl vienu lapu";
+$langCouldNotSendPage = "is fails nav HTML form&#257;t&#257; un netika augupl&#257;d&#275;ts. Ja J&#363;s v&#275;laties nos&#363;t&#299;t ne-HTML dokumentus (PDF, Word, Power Point, Video, utmldz.), izmantojiet <a href=../document/document.php>Dokumentu</a> sada&#316;u";
+$langSendPage = "Lapu, kas jāaug";
+$langPageTitleModified = "Lapas virsraksts tika mainīts";
+$langPageAdded = "Lapa pievienota";
+$langAddPage = "Pievienot lapu";
+?>
