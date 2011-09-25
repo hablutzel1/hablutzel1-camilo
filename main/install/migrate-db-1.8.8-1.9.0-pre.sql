907a260195e0355c2120cdbe2db03f56a9c79c86
ywarnier <ywarnier@localhost>
2011-09-24 20:50:38 -0500

    Updated changelog to prepare 1.9

---
+++ main/install/migrate-db-1.8.8-1.9.0-pre.sql
@@ -0,0 +1,42 @@
+-- This script updates the databases structure before migrating the data from
+-- version 1.8.8 (or version 1.8.8.2, 1.8.8.4) to version 1.9.0
+-- it is intended as a standalone script, however, because of the multiple
+-- databases related difficulties, it should be parsed by a PHP script in
+-- order to connect to and update the right databases.
+-- There is one line per query, allowing the PHP function file() to read
+-- all lines separately into an array. The xxMAINxx-type markers are there
+-- to tell the PHP script which database we're talking about.
+-- By always using the keyword "TABLE" in the queries, we should be able
+-- to retrieve and modify the table name from the PHP script if needed, which
+-- will allow us to deal with the unique-database-type installations
+--
+-- This first part is for the main database
+
+-- xxMAINxx
+UPDATE settings_current SET selected_value = '1.9.0.15650' WHERE variable = 'chamilo_database_version';
+
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('filter_terms', NULL, 'textarea', 'Security', '', 'FilterTermsTitle', 'FilterTermsComment', NULL, NULL, 0);
+
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('header_extra_content', NULL, 'textarea', 'Tracking', '', 'HeaderExtraContentTitle', 'HeaderExtraContentComment', NULL, NULL, 1);
+
+ALTER TABLE personal_agenda ADD COLUMN all_day INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE sys_calendar ADD COLUMN all_day INTEGER NOT NULL DEFAULT 0;
+
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('show_documents_preview', NULL, 'radio', 'Tools', 'false', 'ShowDocumentPreviewTitle', 'ShowDocumentPreviewComment', NULL, NULL, 1);
+INSERT INTO settings_options (variable, value, display_text) VALUES ('show_documents_preview', 'true', 'Yes');
+INSERT INTO settings_options (variable, value, display_text) VALUES ('show_documents_preview', 'false', 'No');
+
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('htmlpurifier_wiki',NULL,'radio','Editor','false','HtmlPurifierWikiTitle','HtmlPurifierWikiComment',NULL,NULL, 0);
+
+INSERT INTO settings_options (variable, value, display_text) VALUES ('htmlpurifier_wiki', 'true', 'Yes');
+INSERT INTO settings_options (variable, value, display_text) VALUES ('htmlpurifier_wiki', 'false', 'No');
+
+
+-- xxSTATSxx
+ALTER TABLE track_e_exercices ADD COLUMN questions_to_check TEXT  NOT NULL DEFAULT '';
+--CREATE TABLE track_filtered_terms (id int, user_id int, course_id int, session_id int, tool_id char(12), filtered_term varchar(255), created_at datetime);
+
+-- xxUSERxx
+
+-- xxCOURSExx
+
---
+++ main/template/default/layout/header.tpl
@@ -0,0 +1,55 @@
+<!-- Topbar -->
+
+{if $_u.status == 1}
+	<div class="topbar">
+	    <div class="topbar-inner">
+	        <div class="container-fluid">
+		        <h3><a href="#">Chamilo</a></h3>
+		        <ul class="nav">
+		            <li class="active"><a href="#">Home</a></li>
+		            <li><a href="#">Profile</a></li>
+		            <li><a href="#">Messages</a></li>		            
+		            <li><a href="#">Administration</a></li>
+		            <li class="dropdown">
+		                <a class="dropdown-toggle" href="#">Dropdown</a>
+		            <ul class="dropdown-menu">
+		                <li><a href="#">Secondary link</a></li>
+		                <li><a href="#">Something else here</a></li>
+		                <li class="divider"></li>
+		                <li><a href="#">Another link</a></li>
+		                </ul>
+		                </li>
+		            </ul>
+		            <form action="">
+		            <input type="text" placeholder="Search">
+		            </form>
+		            <ul class="nav secondary-nav">
+		                <li class="dropdown">
+		                <a class="dropdown-toggle" href="#"><img src="{$_u.avatar_small}"/>  {$_u.complete_name}</a>
+		                <ul class="dropdown-menu">
+		                    <li><a href="#">Settings</a></li>
+		                    <li><a href="#">Help</a></li>
+		                    <li class="divider"></li>		                    
+		                </ul>
+		            </li>
+		            <li><a href="#">Logout</a></li>
+		            </ul>
+			</div>
+		</div><!-- /topbar-inner -->
+	</div><!-- /topbar -->
+	<div id="topbar_push"></div>
+{/if}
+    
+<div id="header">
+	{* header *}
+	{$header1}
+	
+	{* header right *}
+	{$header2}
+	
+	{* menu *}
+	{$header3}
+	
+	{* breadcrumb *}
+	{$header4}
+</div>	
