c4e7315b446e349203cec542ec1255ed07626111
Hubert Borderiou <baelmyhu@gmail.com>
2011-12-12 11:18:04 +0100

    Add username after firstname lastname to avoid homonymous for AGENDA tool -ref #4030

---
+++ main/exercice/exercise_result.php
@@ -0,0 +1,251 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+*	Exercise result
+*	This script gets informations from the script "exercise_submit.php",
+*	through the session, and calculates the score of the student for
+*	that exercise.
+*	Then it shows the results on the screen.
+*	@package chamilo.exercise
+*	@author Olivier Brouckaert, main author
+*	@author Roan Embrechts, some refactoring
+* 	@author Julio Montoya Armas switchable fill in blank option added
+*
+*	@todo	split more code up in functions, move functions to library?
+*/
+/**
+ * Code
+ */
+require_once 'exercise.class.php';
+require_once 'question.class.php';
+require_once 'answer.class.php';
+
+// Name of the language file that needs to be included
+$language_file = 'exercice';
+
+require_once '../inc/global.inc.php';
+require_once 'exercise.lib.php';
+
+if ($_GET['origin']=='learnpath') {
+	require_once '../newscorm/learnpath.class.php';
+	require_once '../newscorm/learnpathItem.class.php';
+	require_once '../newscorm/scorm.class.php';
+	require_once '../newscorm/scormItem.class.php';
+	require_once '../newscorm/aicc.class.php';
+	require_once '../newscorm/aiccItem.class.php';
+}
+require_once api_get_path(LIBRARY_PATH).'exercise_show_functions.lib.php';
+require_once api_get_path(LIBRARY_PATH).'mail.lib.inc.php';
+
+$this_section = SECTION_COURSES;
+
+/* 	ACCESS RIGHTS  */
+api_protect_course_script(true);
+
+// Database table definitions
+$main_admin_table       = Database::get_main_table(TABLE_MAIN_ADMIN);
+
+if($debug>0){error_log('Entered exercise_result.php: '.print_r($_POST,1));}
+
+// general parameters passed via POST/GET
+if ( empty ( $origin ) ) {                  $origin                 = Security::remove_XSS($_REQUEST['origin']);}
+if ( empty ( $objExercise ) ) {             $objExercise            = $_SESSION['objExercise'];}
+if ( empty ( $remind_list ) ) {             $remind_list            = $_REQUEST['remind_list'];}
+
+$exe_id = isset($_REQUEST['exe_id']) ? intval($_REQUEST['exe_id']) : 0;
+
+if (empty($objExercise)) {
+	
+	//Redirect to the exercise overview
+	//Check if the exe_id exists
+	$objExercise = new Exercise();
+	$exercise_stat_info = $objExercise->get_stat_track_exercise_info_by_exe_id($exe_id);
+	if (!empty($exercise_stat_info) && isset($exercise_stat_info['exe_exo_id'])) {
+		header("Location: overview.php?exerciseId=".$exercise_stat_info['exe_exo_id']);
+		exit;
+	}	
+    api_not_allowed();
+}
+
+$gradebook = '';
+if (isset($_SESSION['gradebook'])) {
+	$gradebook=	$_SESSION['gradebook'];
+}
+if (!empty($gradebook) && $gradebook=='view') {
+	$interbreadcrumb[]= array ('url' => '../gradebook/'.$_SESSION['gradebook_dest'], 'name' => get_lang('ToolGradebook'));
+}
+
+$nameTools = get_lang('Exercice');
+
+$interbreadcrumb[]= array("url" => "exercice.php?gradebook=$gradebook","name" => get_lang('Exercices'));
+
+if ($origin != 'learnpath') {
+	//so we are not in learnpath tool
+	Display::display_header($nameTools,get_lang('Exercise'));
+} else {
+    Display::display_reduced_header();
+}
+
+//Hide results
+$show_results     = false;
+$show_only_score  = false;
+
+if ($objExercise->results_disabled == 0) {
+    $show_results = true;	
+}
+
+if ($objExercise->results_disabled == 2) {
+    $show_only_score = true;
+}
+
+/* DISPLAY AND MAIN PROCESS */
+
+// I'm in a preview mode as course admin. Display the action menu.
+if (api_is_course_admin() && $origin != 'learnpath') {
+	echo '<div class="actions">';
+	echo '<a href="admin.php?'.api_get_cidreq().'&exerciseId='.$objExercise->id.'">'.Display::return_icon('back.png', get_lang('GoBackToQuestionList'), array(), 32).'</a>';
+	echo '<a href="exercise_admin.php?'.api_get_cidreq().'&modifyExercise=yes&exerciseId='.$objExercise->id.'">'.Display::return_icon('edit.png', get_lang('ModifyExercise'), array(), 32).'</a>';
+	echo '</div>';
+}
+
+$feedback_type           = $objExercise->feedbacktype;
+$exercise_stat_info      = $objExercise->get_stat_track_exercise_info_by_exe_id($exe_id);
+
+if (!empty($exercise_stat_info['data_tracking'])) {
+	$question_list		= explode(',', $exercise_stat_info['data_tracking']);
+}
+
+$safe_lp_id              = $exercise_stat_info['orig_lp_id'];
+$safe_lp_item_id         = $exercise_stat_info['orig_lp_item_id'];
+$safe_lp_item_view_id    = $exercise_stat_info['orig_lp_item_view_id'];
+
+if ($origin == 'learnpath') {		
+?>
+	<form method="get" action="exercice.php?<?php echo api_get_cidreq() ?>">
+	<input type="hidden" name="origin" 					value="<?php echo $origin; ?>" />
+    <input type="hidden" name="learnpath_id" 			value="<?php echo $safe_lp_id; ?>" />
+    <input type="hidden" name="learnpath_item_id" 		value="<?php echo $safe_lp_item_id; ?>" />
+    <input type="hidden" name="learnpath_item_view_id"  value="<?php echo $safe_lp_item_view_id; ?>" />
+<?php
+}
+
+$i = $total_score = $total_weight = 0;
+
+//We check if the user attempts before sending to the exercise_result.php                
+if ($objExercise->selectAttempts() > 0) {
+    $attempt_count = get_attempt_count(api_get_user_id(), $objExercise->id, $safe_lp_id, $safe_lp_item_id, $safe_lp_item_view_id);                
+    if ($attempt_count >= $objExercise->selectAttempts()) {        
+        Display :: display_warning_message(sprintf(get_lang('ReachedMaxAttempts'), $objExercise->selectTitle(), $objExercise->selectAttempts()), false);
+        if ($origin != 'learnpath') {
+            //we are not in learnpath tool
+            Display::display_footer();
+        }                                      
+        exit;
+    }
+}
+
+
+$user_info   = api_get_user_info(api_get_user_id());     
+if ($show_results || $show_only_score) {
+    echo $exercise_header = $objExercise->show_exercise_result_header(api_get_person_name($user_info['firstName'], $user_info['lastName']));
+}
+
+Display :: display_confirmation_message(get_lang('Saved').'<br />',false);
+
+// Display text when test is finished #4074
+$end_of_message = $objExercise->selectTextWhenFinished();
+if (!empty($end_of_message)) {
+    Display::display_message($end_of_message);
+    echo "<div class='clear'>&nbsp;</div>";
+}
+
+$counter = 1;
+// Loop over all question to show results for each of them, one by one
+if (!empty($question_list)) {
+	foreach ($question_list as $questionId) {
+	    // destruction of the Question object
+		unset($objQuestionTmp);
+		
+		// gets the student choice for this question
+		$choice                = $exerciseResult[$questionId];
+	    
+		// creates a temporary Question object
+		$objQuestionTmp        = Question :: read($questionId);
+			
+		//this variable commes from exercise_submit_modal.php	
+	
+		//$hotspot_delineation_result = $_SESSION['hotspot_delineation_result'][$objExercise->selectId()][$quesId]; 
+		
+		if ($show_results) {
+		    // show category
+		    Testcategory::displayCategoryAndTitle($objQuestionTmp->id);
+	    	// show titles    	
+	    	echo $objQuestionTmp->return_header($objExercise->feedback_type, $counter);
+	    	$counter++;    	
+		}
+		
+	    // We're inside *one* question. Go through each possible answer for this question
+	    $result = $objExercise->manage_answer($exercise_stat_info['exe_id'], $questionId, null ,'exercise_result', array(), false, true, $show_results, $objExercise->selectPropagateNeg(), $hotspot_delineation_result);	    
+	   	
+	    $total_score     += $result['score'];    
+	    $total_weight    += $result['weight'];    
+	} // end foreach() block that loops over all questions
+}
+
+if ($origin != 'learnpath') {
+    if ($show_results || $show_only_score) {
+        echo '<div id="question_score">';
+        echo get_lang('YourTotalScore')." ";	
+        if ($objExercise->selectPropagateNeg() == 0 && $total_score < 0) {
+    	    $total_score = 0;
+        }     
+        echo show_score($total_score, $total_weight, false);	
+        echo '</div>';
+    }
+    /* <button type="submit" class="save"><?php echo get_lang('Finish');?></button> */
+}
+
+// Tracking of results
+
+//	Updates the empty exercise
+
+$quizDuration = (!empty($_SESSION['quizStartTime']) ? time() - $_SESSION['quizStartTime'] : 0);
+
+$feed = $objExercise->feedbacktype; 
+if (api_is_allowed_to_session_edit()) {	
+	update_event_exercice($exercise_stat_info['exe_id'], $objExercise->selectId(), $total_score, $total_weight, api_get_session_id(), $safe_lp_id, $safe_lp_item_id, $safe_lp_item_view_id, $quiz_duration, $question_list, '');	
+}
+
+
+//If is not valid
+$session_control_key = get_session_time_control_key($objExercise->id);
+if (isset($session_control_key) && !exercise_time_control_is_valid($objExercise->id)) {
+	$TBL_TRACK_ATTEMPT		= Database::get_statistic_table(TABLE_STATISTIC_TRACK_E_ATTEMPT);
+	$sql_fraud = "UPDATE $TBL_TRACK_ATTEMPT SET answer = 0, marks=0, position=0 WHERE exe_id = $exe_id ";
+	Database::query($sql_fraud);
+}
+
+//Unset session for clock time
+exercise_time_control_delete($objExercise->id);
+
+if ($origin != 'learnpath') {	
+	Display::display_footer();
+} else {
+	$lp_mode =  $_SESSION['lp_mode'];
+	$url = '../newscorm/lp_controller.php?cidReq='.api_get_course_id().'&action=view&lp_id='.$safe_lp_id.'&lp_item_id='.$safe_lp_item_id.'&exeId='.$exercise_stat_info['exe_id'].'&fb_type='.$feed;
+	//echo $total_score.','.$total_weight;	exit;	
+	$href = ($lp_mode == 'fullscreen')?' window.opener.location.href="'.$url.'" ':' top.location.href="'.$url.'" ';
+	echo '<script language="javascript" type="text/javascript">'.$href.'</script>'."\n";
+	//record the results in the learning path, using the SCORM interface (API)
+	echo '<script language="javascript" type="text/javascript">window.parent.API.void_save_asset('.$total_score.','.$total_weight.');</script>'."\n";
+	echo '</body></html>';
+}
+
+// Send notification..
+if (!api_is_allowed_to_edit(null,true)) {	
+    $objExercise->send_notification($arrques, $arrans, $origin);	
+}
+if (api_is_allowed_to_session_edit()) {
+	api_session_unregister('objExercise');
+	api_session_unregister('exe_id');
+}
---
+++ main/gradebook/gradebook_flatview.php
@@ -0,0 +1,336 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * Script
+ * @package chamilo.gradebook
+ */
+/**
+ * Init
+ */
+
+$language_file = 'gradebook';
+require_once '../inc/global.inc.php';
+require_once 'lib/be.inc.php';      
+require_once 'lib/fe/dataform.class.php';
+require_once 'lib/fe/userform.class.php';
+require_once 'lib/flatview_data_generator.class.php';
+require_once 'lib/fe/flatviewtable.class.php';
+require_once 'lib/fe/displaygradebook.php';
+require_once 'lib/fe/exportgradebook.php';
+require_once 'lib/scoredisplay.class.php';
+require_once api_get_path(SYS_CODE_PATH).'gradebook/lib/gradebook_functions.inc.php';
+require_once api_get_path(LIBRARY_PATH).'pdf.lib.php';
+
+api_block_anonymous_users();
+block_students();
+
+if (isset ($_POST['submit']) && isset ($_POST['keyword'])) {
+	header('Location: '.api_get_self().'?selectcat='.Security::remove_XSS($_GET['selectcat']).'&search='.Security::remove_XSS($_POST['keyword']));
+	exit;
+}
+
+$interbreadcrumb[] = array ('url' => $_SESSION['gradebook_dest'].'?selectcat=1', 'name' => get_lang('ToolGradebook'));
+
+$showeval = isset($_POST['showeval']) ? '1' : '0';
+$showlink = isset($_POST['showlink']) ? '1' : '0';
+if (($showlink == '0') && ($showeval == '0')) {
+	$showlink = '1';
+	$showeval = '1';
+}
+$cat = Category::load($_REQUEST['selectcat']);
+
+if (isset($_GET['userid'])) {
+	$userid = Security::remove_XSS($_GET['userid']);
+} else {
+	$userid = '';
+}
+
+if ($showeval) {
+	$alleval = $cat[0]->get_evaluations($userid, true);
+} else {
+	$alleval = null;
+}
+
+if ($showlink) {
+	$alllinks = $cat[0]->get_links($userid, true);
+} else {
+	$alllinks = null;
+}
+
+if (isset($export_flatview_form) && (!$file_type == 'pdf')) {
+	Display :: display_normal_message($export_flatview_form->toHtml(), false);
+}
+
+if (isset($_GET['selectcat'])) {
+	$category_id = Security::remove_XSS($_GET['selectcat']);
+} else {
+	$category_id = '';
+}
+
+$simple_search_form = new UserForm(UserForm :: TYPE_SIMPLE_SEARCH, null, 'simple_search_form', null, api_get_self() . '?selectcat=' . $category_id);
+$values = $simple_search_form->exportValues();
+
+$keyword = '';
+if (isset($_GET['search']) && !empty($_GET['search'])) {
+	$keyword = Security::remove_XSS($_GET['search']);
+}
+if ($simple_search_form->validate() && (empty($keyword))) {
+	$keyword = $values['keyword'];
+}
+
+if (!empty($keyword)) {
+	$users = find_students($keyword);
+} else {
+	if (isset($alleval) && isset($alllinks)) {
+		$users = get_all_users($alleval, $alllinks);
+	} else {
+		$users = null;
+	}
+}
+
+$offset = isset($_GET['offset']) ? $_GET['offset'] : '0';
+$flatviewtable = new FlatViewTable($cat[0], $users, $alleval, $alllinks, true, $offset, $addparams);
+$parameters=array('selectcat'=>intval($_GET['selectcat']));
+$flatviewtable->set_additional_parameters($parameters);
+
+if (isset($_GET['exportpdf']))	{
+	$interbreadcrumb[] = array (
+		'url' => api_get_self().'?selectcat=' . Security::remove_XSS($_GET['selectcat']),
+		'name' => get_lang('FlatView')
+	);
+
+	$export_pdf_form = new DataForm(DataForm::TYPE_EXPORT_PDF, 'export_pdf_form', null, api_get_self().'?exportpdf=&offset='.intval($_GET['offset']).'&selectcat='.intval($_GET['selectcat']), '_blank', '');
+
+	if ($export_pdf_form->validate()) {
+
+		// Beginning of PDF report creation
+
+		$printable_data = get_printable_data($cat[0], $users, $alleval, $alllinks);
+		$export = $export_pdf_form->exportValues();
+
+		// Reading report's CSS
+
+		//$css_file = api_get_path(TO_SYS, WEB_CSS_PATH).api_get_setting('stylesheets').'/print.css';
+		$css_file = api_get_path(SYS_CODE_PATH).'gradebook/print.css';
+		$css = file_exists($css_file) ? @file_get_contents($css_file) : '';
+
+		// HTML report creation first
+
+		$time = time();
+		$cat_name = trim($cat[0]->get_name());
+		$course_code = trim($cat[0]->get_course_code());
+		$organization = api_get_setting('Institution');
+		
+		$displayscore = ScoreDisplay :: instance();
+		$customdisplays = $displayscore->get_custom_score_display_settings();
+		
+		if (is_array($customdisplays) && count(($customdisplays))) {
+			$total = array();
+			foreach($customdisplays  as $custom) {
+				$total[$custom['display']]  = 0; 
+ 			}			
+			$user_results = $flatviewtable->datagen->get_data_to_graph2();
+			foreach($user_results  as $user_result) {
+				$total[$user_result[count($user_result)-1][1]]++;
+			}
+		}
+		
+		$html = '';
+		
+		$img = api_get_path(SYS_CODE_PATH).'css/'.api_get_visual_theme().'/images/header-logo.png';
+		if (file_exists($img)) {
+			$img = api_get_path(WEB_CODE_PATH).'css/'.api_get_visual_theme().'/images/header-logo.png';
+			$html .= "<img src='$img'>";			
+		} else {
+			if (!empty($organization)) {			  
+				$html .= '<h2 align="left">'.$organization.'</h2>';
+			}
+		}
+		$html .= '<h2 align="center">'.get_lang('FlatView').'</h2>';
+		
+		$html .= '<table align="center" width="100%"><tr><td valign="top">';
+		
+		$html .= '<table align="left" width="33%">';		
+		$session_name = api_get_session_name(api_get_session_id());
+		$teacher_list = CourseManager::get_teacher_list_from_course_code_to_string($course_code);
+		if (!empty($session_name)) {
+			$html .= Display::tag('tr', Display::tag('td', get_lang('Session')).Display::tag('td', Display::tag('strong', $session_name)));
+		}		
+		$html .= Display::tag('tr', Display::tag('td', get_lang('Course')).Display::tag('td', Display::tag('strong', $course_code)));
+		$html .= Display::tag('tr', Display::tag('td', get_lang('Date')).Display::tag('td', Display::tag('strong', api_convert_and_format_date(date('Y-m-d', time()), DATE_TIME_FORMAT_LONG))));
+		$html .= Display::tag('tr', Display::tag('td', get_lang('Teacher')).Display::tag('td', Display::tag('strong', $teacher_list)));
+		$html .= '</table></td>';
+		
+		$html .= '<td valign="top"><table align="left" width="33%">';
+		
+		if (!empty($total)) {			
+			foreach($total as $label => $count) {
+				$total_custom_score = round($count/count($user_results), 2) *100;
+				$html .= Display::tag('tr', Display::tag('td', $label).': '.Display::tag('td', Display::tag('strong', $total_custom_score.' %')));
+			}
+		}
+		$html .= '</table></td>';		
+		$html .= '<td valign="top"><table align="left" width="33%">';
+		$headers = $printable_data[0];
+		unset($headers[0]);
+		unset($headers[1]);
+		unset($headers[count($headers)+1]);
+		
+		foreach ($headers as $head) {
+			//$html .= Display::tag('tr', Display::tag('td', 'P1').Display::tag('td', Display::tag('strong', $head)));		
+			$html .= Display::tag('tr', Display::tag('td', Display::tag('strong', $head)));
+		}		
+		$html .= '</table></td></table><br />';		
+
+		$columns = count($printable_data[0]);
+		$has_data = is_array($printable_data[1]) && count($printable_data[1]) > 0;
+
+		if (api_is_western_name_order()) {
+			// Choosing the right person name order according to the current language.
+			list($printable_data[0][0], $printable_data[0][1]) = array($printable_data[0][1], $printable_data[0][0]);
+			if ($has_data) {
+				foreach ($printable_data[1] as &$printable_data_row) {
+					list($printable_data_row[0], $printable_data_row[1]) = array($printable_data_row[1], $printable_data_row[0]);
+				}
+			}
+		}
+
+		$table = new HTML_Table(array('class' => 'data_table'));
+		$row = 0;
+		$column = 0;
+		foreach ($printable_data[0] as $printable_data_cell) {
+			$table->setHeaderContents($row, $column, $printable_data_cell);
+			$column++;
+		}
+		$row++;
+		if ($has_data) {
+			foreach ($printable_data[1] as &$printable_data_row) {
+				$column = 0;
+				foreach ($printable_data_row as &$printable_data_cell) {
+					$table->setCellContents($row, $column, $printable_data_cell);
+					$table->updateCellAttributes($row, $column, 'align="center"');
+					$column++;
+				}
+				$table->updateRowAttributes($row, $row % 2 ? 'class="row_even"' : 'class="row_odd"', true);
+				$row++;
+			}
+		} else {
+			$column = 0;
+			$table->setCellContents($row, $column, get_lang('NoResults'));
+			$table->updateCellAttributes($row, $column, 'colspan="'.$columns.'" align="center" class="row_odd"');
+		}
+
+		$html .= $table->toHtml();
+		
+		//echo $html;exit;
+		// Memory release
+
+		unset($printable_data);
+		unset($table);
+
+		// Conversion of the created HTML report to a PDF report
+
+		$html         = api_utf8_encode($html);
+        //@todo this is really a must?
+		$creator_pdf  = api_utf8_encode($creator);
+		$title_pdf    = api_utf8_encode($report_name);
+		$subject_pdf  = api_utf8_encode(get_lang('FlatView'));
+		$keywods_pdf  = api_utf8_encode($course_code);
+
+		$page_format = $export['orientation'] == 'landscape' ? 'A4-L' : 'A4';
+        $pdf = new PDF($page_format, $export['orientation']);
+        
+        // Sending the created PDF report to the client
+        $file_name = date('YmdHi_', $time);
+        if (!empty($course_code)) {
+            $file_name .= $course_code.'_';
+        }
+        $file_name .= get_lang('FlatView').'.pdf';
+        $pdf->content_to_pdf($html, $css, $file_name, api_get_course_id());
+        exit;	
+	} else {
+		Display :: display_header(get_lang('ExportPDF'));
+	}
+}
+
+if (isset ($_GET['print']))	{
+	$printable_data = get_printable_data ($cat[0], $users,$alleval, $alllinks);
+	echo print_table($printable_data[1],$printable_data[0], get_lang('FlatView'), $cat[0]->get_name());
+	exit;
+}
+
+
+        
+        
+if (!empty($_GET['export_report']) && $_GET['export_report'] == 'export_report') {    
+	if (api_is_platform_admin() || api_is_course_admin() || api_is_course_coach()) {
+		$user_id = null;
+
+		if (empty($_SESSION['export_user_fields'])) {
+			$_SESSION['export_user_fields'] = false;
+		}
+		if (!api_is_allowed_to_edit(false, false) and !api_is_course_tutor()) {
+			$user_id = api_get_user_id();
+		}
+
+		require_once 'gradebook_result.class.php';
+		$printable_data = get_printable_data($cat[0], $users, $alleval, $alllinks);
+        
+		switch($_GET['export_format']) {
+			case 'xls':
+				$export = new GradeBookResult();
+				$export->exportCompleteReportXLS($printable_data);
+				break;
+			case 'doc':
+				$export = new GradeBookResult();
+				$export->exportCompleteReportDOC($printable_data);
+                exit;
+				break;
+			case 'csv':
+			default:
+				$export = new GradeBookResult();
+				$export->exportCompleteReportCSV($printable_data);
+				exit;
+				break;
+		}
+	} else {
+		api_not_allowed(true);
+	}
+}
+
+$addparams = array ('selectcat' => $cat[0]->get_id());
+if (isset($_GET['search'])) {
+	$addparams['search'] = $keyword;
+}
+
+
+$this_section = SECTION_COURSES;
+
+if (isset($_GET['exportpdf'])) {
+	echo '<div class="normal-message">';
+	$export_pdf_form->display();
+	echo '</div>';
+} else {
+	Display :: display_header(get_lang('FlatView'));
+}
+
+if (isset($_GET['isStudentView']) && $_GET['isStudentView'] == 'false') {
+	DisplayGradebook :: display_header_reduce_flatview($cat[0], $showeval, $showlink, $simple_search_form);
+	$flatviewtable->display();
+} elseif (isset($_GET['selectcat']) && ($_SESSION['studentview'] == 'teacherview')) {
+	DisplayGradebook :: display_header_reduce_flatview($cat[0], $showeval, $showlink, $simple_search_form);
+	
+	// main graph
+	$flatviewtable->display();	
+	// @todo this needs a fix
+	//$image_file = $flatviewtable->display_graph();
+	//@todo load images with jquery
+    echo '<div id="contentArea" style="text-align: center;" >';        		
+	if (!empty($image_file)) {
+		echo '<img src="'.$image_file.'">';
+	}        
+	$flatviewtable->display_graph_by_resource();
+	echo '</div>';
+}
+
+Display :: display_footer();
