d7d111771b20e8a3dc30f717917dcdffb7b94b5c
Laurent Opprecht <Laurent Opprecht@localhost>
2012-05-04 11:56:24 +0200

    migrate can't drop index unique settings #4703

---
+++ plugin/bbb/lang/english.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+
+//Needed in order to show the plugin title
+$strings['plugin_title']        = "BigBlueButton";
+$strings['plugin_comment']      = "Open Source Videoconference tool";
+
+
+$strings['Videoconference']     = "Videoconference";
+$strings['MeetingOpened']       = "Meeting opened";
+$strings['MeetingClosed']       = "Meeting closed";
+$strings['CloseMeeting']        = "Close meeting";
+
+$strings['VideoConferenceXCourseX']             = "Videoconference #%s course %s";
+$strings['VideoConferenceAddedToTheCalendar']   = "Videoconference added to the calendar";
+
+$strings['Records']             = "Records";
+$strings['Meeting']             = "Meeting";
+
+$strings['ViewRecord']          = "View record";
+$strings['CopyToLinkTool']      = "Copy to link tool";
+
+$strings['StartConference']     = "Start conference";
+$strings['RecordList']          = "Record list";
+$strings['ServerIsNotRunning']  = "Videoconference server is not running";
+$strings['XUsersOnLine']        = "%s user(s) online";
