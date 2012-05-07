41b8a1749ae0dc1f905dc678b35faa8a415abc3d
Hubert Borderiou <baelmyhu@gmail.com>
2012-05-07 15:28:38 +0200

    Add Shibbolet and Facebook auth configuration details for Chamilo admin conf

---
+++ plugin/bbb/lang/english.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+
+//Needed in order to show the plugin title
+$strings['plugin_title']        = "BigBlueButton";
+$strings['plugin_comment']      = "Open Source Videoconference tool";
+
+$strings['Videoconference']     = "Videoconference";
+$strings['MeetingOpened']       = "Meeting opened";
+$strings['MeetingClosed']       = "Meeting closed";
+$strings['CloseMeeting']        = "Close meeting";
+
+$strings['VideoConferenceXCourseX']             = "Videoconference #%s course %s";
+$strings['VideoConferenceAddedToTheCalendar']   = "Videoconference added to the calendar";
+$strings['VideoConferenceAddedToTheLinkTool']   = "Videoconference added to the link tool";
+
+$strings['GoToTheVideoConference']   = "Go to the videoconference";
+
+$strings['Records']             = "Records";
+$strings['Meeting']             = "Meeting";
+
+$strings['ViewRecord']          = "View record";
+$strings['CopyToLinkTool']      = "Copy to link tool";
+
+$strings['EnterConference']     = "Enter to the videoconference";
+$strings['RecordList']          = "Record list";
+$strings['ServerIsNotRunning']  = "Videoconference server is not running";
+$strings['ServerIsNotConfigured']  = "Videoconference server is not configured";
+
+$strings['XUsersOnLine']        = "%s user(s) online";
