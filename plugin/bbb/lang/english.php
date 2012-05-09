399df3ba14592bfdcd067a963ac6679862666e73
Julio Montoya <gugli100@gmail.com>
2012-05-09 14:27:34 +0200

    Moving code inside the lib folder, moving install, uninstall code in a class see BT#3123

---
+++ plugin/bbb/lang/english.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+
+//Needed in order to show the plugin title
+$strings['plugin_title']        = "BigBlueButton (BBB)";
+$strings['plugin_comment']      = "Adds a videoconference room Chamilo courses";
+
+$strings['Videoconference']     = "Videoconference";
+$strings['MeetingOpened']       = "Meeting opened";
+$strings['MeetingClosed']       = "Meeting closed";
+$strings['CloseMeeting']        = "Close meeting";
+
+$strings['VideoConferenceXCourseX']             = "Videoconference #%s course %s";
+$strings['VideoConferenceAddedToTheCalendar']   = "Videoconference added to the calendar";
+$strings['VideoConferenceAddedToTheLinkTool']   = "Videoconference added to the link tool";
+
+$strings['GoToTheVideoConference']   = "Go to the videoconference";
+
+$strings['Records']             = "Records";
+$strings['Meeting']             = "Meeting";
+
+$strings['ViewRecord']          = "View record";
+$strings['CopyToLinkTool']      = "Copy to link tool";
+
+$strings['EnterConference']     = "Enter to the videoconference";
+$strings['RecordList']          = "Record list";
+$strings['ServerIsNotRunning']  = "Videoconference server is not running";
+$strings['ServerIsNotConfigured']  = "Videoconference server is not configured";
+
+$strings['XUsersOnLine']        = "%s user(s) online";
