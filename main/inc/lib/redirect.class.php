44b328d44bead6ee10199df3343d418636724e01
Julio Montoya <gugli100@gmail.com>
2012-05-22 15:24:43 +0200

    Adding grading model when creating a course if settings are on see BT#4080

---
+++ main/inc/lib/redirect.class.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Send a redirect to the user agent and exist
+ * 
+ * @license see /license.txt
+ * @author Laurent Opprecht <laurent@opprecht.info> for the Univesity of Geneva
+ */
+class Redirect
+{
+
+    static function www()
+    {
+        return Uri::www();
+    }
+
+    static function go($url = '')
+    {
+        if (empty($url))
+        {
+            Redirect::session_request_uri();
+            $www = self::www();
+            self::navigate($www);
+        }
+
+        $is_full_uri = (strpos($url, 'http') === 0);
+        if ($is_full_uri)
+        {
+            self::navigate($url);
+        }
+
+        $url = self::www() . $url;
+        self::navigate($url);
+    }
+
+    /**
+     * Redirect to the session "request uri" if it exists. 
+     */
+    static function session_request_uri()
+    {
+//        if (api_is_anonymous())
+//        {
+//            return;
+//        }
+        $no_redirection = isset($_SESSION['noredirection']) ? $_SESSION['noredirection'] : false;
+        
+        if($no_redirection){
+            unset($_SESSION['noredirection']);
+            return;
+        }
+        
+        $url = isset($_SESSION['request_uri']) ? $_SESSION['request_uri'] : '';
+        unset($_SESSION['request_uri']);
+        
+        
+        if ($url)
+        {
+            self::navigate($url);
+        }
+    }
+
+    static function home()
+    {
+        $www = self::www();
+        self::navigate($www);
+    }
+
+    static function user_home()
+    {
+        $www = self::www();
+        self::navigate("$www/user_portal.php");
+    }
+
+    protected static function navigate($url)
+    {
+        session_write_close(); //should not be neeeded 
+        header("Location: $url");
+        exit;
+    }
+
+}
---
+++ main/work/downloadfolder.inc.php
@@ -0,0 +1,136 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/**
+ *	Functions and main code for the download folder feature
+ *  @todo use ids instead of the path like the document tool 
+ *	@package chamilo.work
+ */
+
+$work_id = $_GET['id'];
+require_once '../inc/global.inc.php';
+$current_course_tool  = TOOL_STUDENTPUBLICATION;
+
+//protection
+api_protect_course_script(true);
+
+require_once 'work.lib.php';
+
+$work_data = get_work_data_by_id($work_id);
+if (empty($work_data)) {
+    exit;
+}
+
+//prevent some stuff
+if (empty($path)) {
+	$path = '/';
+}
+
+if (empty($_course) || empty($_course['path'])) {
+    api_not_allowed();
+}
+$sys_course_path = api_get_path(SYS_COURSE_PATH);
+
+//zip library for creation of the zipfile
+require_once api_get_path(LIBRARY_PATH).'pclzip/pclzip.lib.php';
+
+//Creating a ZIP file
+$temp_zip_file = api_get_path(SYS_ARCHIVE_PATH).api_get_unique_id().".zip";
+
+$zip_folder = new PclZip($temp_zip_file);
+
+$tbl_student_publication = Database::get_course_table(TABLE_STUDENT_PUBLICATION);
+$prop_table              = Database::get_course_table(TABLE_ITEM_PROPERTY);
+
+//Put the files in the zip
+//2 possibilities: admins get all files and folders in the selected folder (except for the deleted ones)
+//normal users get only visible files that are in visible folders
+
+//admins are allowed to download invisible files
+$files = array();
+$course_id = api_get_course_int_id();
+
+if (api_is_allowed_to_edit()) {
+	//search for all files that are not deleted => visibility != 2
+    
+    $sql = "SELECT url, title FROM $tbl_student_publication AS work, $prop_table AS props  
+ 			WHERE   props.c_id = $course_id AND 
+ 			        work.c_id = $course_id AND 
+ 			        props.tool='work' AND 
+ 			        work.id=props.ref AND 
+ 			        work.parent_id = $work_id AND 
+ 			        work.filetype='file' AND props.visibility<>'2'";
+	$query = Database::query($sql);
+	//add tem to the zip file
+	while ($not_deleted_file = Database::fetch_assoc($query)) {
+		if (file_exists($sys_course_path.$_course['path'].'/'.$not_deleted_file['url'])) {            
+			$files[basename($not_deleted_file['url'])] = $not_deleted_file['title'];
+		    $zip_folder->add($sys_course_path.$_course['path'].'/'.$not_deleted_file['url'], PCLZIP_OPT_REMOVE_PATH, $sys_course_path.$_course['path'].'/work', PCLZIP_CB_PRE_ADD, 'my_pre_add_callback');
+		}
+    }    
+} else {
+    //for other users, we need to create a zipfile with only visible files and folders    
+    $sql = "SELECT url, title FROM $tbl_student_publication AS work, $prop_table AS props  
+            WHERE   props.c_id = $course_id AND work.c_id = $course_id AND 
+                    props.tool='work' AND 
+                    work.accepted = 1 AND 
+                    work.id=props.ref AND 
+                    work.parent_id = $work_id AND
+                    work.filetype='file' AND 
+                    props.visibility = '1' AND props.insert_user_id='".api_get_user_id()."' ";
+    $query = Database::query($sql);
+    //add tem to the zip file
+    while ($not_deleted_file = Database::fetch_assoc($query)) {
+        if (file_exists($sys_course_path.$_course['path'].'/'.$not_deleted_file['url'])) {
+            $files[basename($not_deleted_file['url'])] = $not_deleted_file['title'];
+            $zip_folder->add($sys_course_path.$_course['path'].'/'.$not_deleted_file['url'], PCLZIP_OPT_REMOVE_PATH, $sys_course_path.$_course['path'].'/work', PCLZIP_CB_PRE_ADD, 'my_pre_add_callback');
+        }
+    }
+}//end for other users
+
+if (!empty($files)) {
+    //logging
+    event_download(basename($work_data['title']).'.zip (folder)');
+
+    //start download of created file
+    $name = basename($work_data['title']).'.zip';
+    
+    if (Security::check_abs_path($temp_zip_file, api_get_path(SYS_ARCHIVE_PATH))) {    
+        DocumentManager::file_send_for_download($temp_zip_file, true, $name);    
+        @unlink($temp_zip_file);    
+        exit;    
+    }
+} else {
+    exit;
+}
+
+/*	Extra function (only used here) */
+
+function my_pre_add_callback($p_event, &$p_header) {
+	global $files;	
+	if (isset($files[basename($p_header['stored_filename'])])) {
+		$p_header['stored_filename'] = $files[basename($p_header['stored_filename'])];
+		return 1;
+	}
+	return 0;	
+}
+
+/**
+ * Return the difference between two arrays, as an array of those key/values
+ * Use this as array_diff doesn't give the
+ *
+ * @param array $arr1 first array
+ * @param array $arr2 second array
+ * @return difference between the two arrays
+ */
+function diff($arr1, $arr2) {
+	$res = array();
+	$r = 0;
+	foreach ($arr1 as $av) {
+		if (!in_array($av, $arr2)) {
+			$res[$r] = $av;
+			$r++;
+		}
+	}
+	return $res;
+}
