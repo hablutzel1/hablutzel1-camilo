6b67de4c26d3a532f36909780e8c5fa78c3c06cf
Laurent Opprecht <Laurent Opprecht@localhost>
2012-05-29 14:49:58 +0200

    4784 api session register

---
+++ main/lang/greek/dropbox.inc.php
@@ -0,0 +1,52 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$BackList = "Επιστροφή στο Χώρο Ανταλλαγής Αρχείων";
+$ShowFeedback = "Show Feedback";
+$GiveFeedback = "Give / Edit Feedback";
+$JustUploadInSelect = "--- Ανέβασμα αρχείου ---";
+$MailingNothingFor = "Τίποτε για";
+$MailingFileNotRegistered = "(όχι εγγεγραμμένος)";
+$MailingFileSentTo = "στάλθηκε στον/στην";
+$MailingFileIsFor = "παραλήπτης:";
+$MailingFileRecipDup = "multiple users have";
+$MailingFileRecipNotFound = "no such student with";
+$MailingFileNoRecip = "name does not contain any recipient-id";
+$MailingFileNoPostfix = "name does not end with";
+$MailingFileNoPrefix = "name does not start with";
+$MailingFileFunny = "no name, or extension not 1-4 letters or digits";
+$MailingZipDups = "Mailing zipfile must not contain duplicate files - it will not be sent";
+$MailingZipPhp = "Mailing zipfile must not contain php files - it will not be sent";
+$MailingZipEmptyOrCorrupt = "Mailing zipfile is empty or not a valid zipfile";
+$MailingWrongZipfile = "Mailing must be zipfile with STUDENTID or LOGINNAME";
+$MailingConfirmSend = "Send content files to individual destinations ?";
+$MailingSend = "Send content files";
+$MailingNotYetSent = "Mailing content files have not yet been sent out...";
+$MailingInSelect = "---Mailing---";
+$MailingAsUsername = "Mailing";
+$FileDeleted = "Το επιλεγμένο αρχείο έχει διαγραφεί από το Χώρο Ανταλλαγής Αρχείων.";
+$DocAdd = "Το αρχείο στάλθηκε με επιτυχία";
+$Sender = "τον αποστολέα";
+$Author = "τον διδάσκοντα";
+$FileSize = "το μέγεθος του αρχείου";
+$Title = "τον τίτλο";
+$OverwriteFile = "Θέλετε να αντικαταστήσετε το προηγούμενο αρχείο που στείλατε;";
+$Ok = "Αποστολή";
+$Unknown = "ανώνυμος";
+$SentOn = "την";
+$SentTo = "Στάλθηκε στον/στην";
+$ConfirmDelete = "Σημείωση: Το αρχείο θα διαγραφτεί μόνο από τον κατάλογό σας";
+$SendTo = "Αποστολή στον/στην";
+$Description = "Περιγραφή αρχείου";
+$UploadFile = "Ανέβασμα αρχείου";
+$ErrorCreatingDir = "Παρουσιάστηκε σφάλμα κατά τη δημιουργία καταλόγου. Παρακαλούμε επικοινωνήστε με τον διαχειριστή της πλατφόρμας.";
+$UploadError = "Παρουσιάστηκε σφάλμα κατά το ανέβασμα του αρχείου. Παρακαλούμε επικοινωνήστε με τον διαχειριστή της πλατφόρμας.";
+$TooBig = "Δεν έχετε επιλέξει κάποιο αρχείο να ανεβάσετε ή το αρχείο υπερβαίνει το επιτρεπτό όριο σε μέγεθος.";
+$NoFileSpecified = "Δεν έχετε επιλέξει κάποιο αρχείο για να ανεβάσετε.";
+$NoUserSelected = "Παρακαλούμε επιλέξτε το χρήστη στον οποίο θέλετε να σταλεί το αρχείο.";
+$BadFormData = "Η αποστολή του αρχείου απέτυχε: Τα δεδομένα ήταν με λάθος μορφή. Παρακαλούμε επικοινωνήστε με τον διαχειριστή της πλατφόρμας.";
+$GeneralError = "Παρουσιάστηκε σφάλμα. Παρακαλούμε επικοινωνήστε με τον διαχειριστή της πλατφόρμας.";
+$Dropbox = "Χώρος Ανταλλαγής Αρχείων";
+$Help = "Βοήθεια";
+?>
